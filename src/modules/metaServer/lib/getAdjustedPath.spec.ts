b786cb38b0c06bc7c0cdd4a1427f77fe600b2c11
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-04 12:36:01 -0500

    Add tests for schema in path

--- src/modules/metaServer/lib/getAdjustedPath.spec.ts
+++ src/modules/metaServer/lib/getAdjustedPath.spec.ts
@@ -13,6 +13,9 @@ describe('getAdjustedPath', () => {
   it('returns the real path if there is an extension', () => {
     expect(getAdjustedPath(getRequestForUri('/index.js'))).toBe(`${sep}index.js`)
   })
+  it('returns the real path if there is a schema', () => {
+    expect(getAdjustedPath(getRequestForUri('/network.xyo.payload'))).toBe(`${sep}network.xyo.payload`)
+  })
   it('returns index.html if there is no extension', () => {
     expect(getAdjustedPath(getRequestForUri('/'))).toBe(`${sep}index.html`)
   })

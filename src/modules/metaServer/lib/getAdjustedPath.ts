a3f3522c9265d07f9970aaf3646a08b8feb38311
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-05 16:23:03 -0500

    Remove unnecessary join

--- src/modules/metaServer/lib/getAdjustedPath.ts
+++ src/modules/metaServer/lib/getAdjustedPath.ts
@@ -2,5 +2,5 @@ import { Request } from 'express'
 import { extname, join } from 'path'
 
 export const getAdjustedPath = (req: Request): string => {
-  return extname(req.path).length > 0 ? join(req.path) : join(req.path, 'index.html')
+  return extname(req.path).length > 0 ? req.path : join(req.path, 'index.html')
 }

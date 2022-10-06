f29c89e1e58f93a28c6413fced3d02a70668f458
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-06 07:21:20 -0500

    Tests for getHashInfoFromUri

---
+++ src/modules/metaServer/lib/uri/getHashInfoFromUri.spec.ts
@@ -0,0 +1,56 @@
+import { getHashInfoFromUri } from './getHashInfoFromUri'
+
+describe('getHashInfoFromUri', () => {
+  describe('with valid URI', () => {
+    const uris = [
+      'https://beta.explore.xyo.network/archive/temp/block/hash/18b79aa80de2bcc7287fd0577a5c2caee47a37fd4ac75a252e4bc05aeddd7fbb?network=kerplunk',
+      'https://explore.xyo.network/archive/temp/block/hash/ce9d1723d3f501abc980c4f48248c11c9689051d9a2010d521e4ee484b260436?network=main',
+      '/archive/temp/block/hash/ce9d1723d3f501abc980c4f48248c11c9689051d9a2010d521e4ee484b260436?network=main',
+      'http://locahost:8080/archive/temp/payload/hash/62378096c541bda4a150643314fb0ed85d6f964023452f586d0e5c74db08d852',
+    ]
+    it.each(uris)('gets hash info from URI', (uri) => {
+      const info = getHashInfoFromUri(uri)
+      expect(info.archive).toBeTruthy()
+      expect(info.hash).toBeTruthy()
+      expect(info.type).toBeTruthy()
+    })
+  })
+  describe('when missing', () => {
+    const domainUris = [
+      'https://explore.xyo.network/?network=main',
+      'https://beta.explore.xyo.network/?network=main',
+      'https://explore.xyo.network/?network=kerplunk',
+      'https://beta.explore.xyo.network/?network=kerplunk',
+    ]
+    const huriUris = [
+      '/ce9d1723d3f501abc980c4f48248c11c9689051d9a2010d521e4ee484b260436?network=main',
+      'https://explore.xyo.network/ce9d1723d3f501abc980c4f48248c11c9689051d9a2010d521e4ee484b260436',
+      'https://explore.xyo.network/ce9d1723d3f501abc980c4f48248c11c9689051d9a2010d521e4ee484b260436?network=main',
+      'https://explore.xyo.network/ce9d1723d3f501abc980c4f48248c11c9689051d9a2010d521e4ee484b260436?network=kerplunk',
+      'https://beta.explore.xyo.network/ce9d1723d3f501abc980c4f48248c11c9689051d9a2010d521e4ee484b260436',
+      'https://beta.explore.xyo.network/ce9d1723d3f501abc980c4f48248c11c9689051d9a2010d521e4ee484b260436?network=main',
+      'https://beta.explore.xyo.network/ce9d1723d3f501abc980c4f48248c11c9689051d9a2010d521e4ee484b260436?network=kerplunk',
+    ]
+    describe('archive', () => {
+      const uris = [...domainUris, ...huriUris, 'https://beta.explore.xyo.network/archive']
+      it.each(uris)('returns undefined', (uri) => {
+        const info = getHashInfoFromUri(uri)
+        expect(info.archive).toBeUndefined()
+      })
+    })
+    describe('hash', () => {
+      const uris = ['https://beta.explore.xyo.network/archive', 'https://explore.xyo.network/?network=main']
+      it.each(uris)('returns undefined', (uri) => {
+        const info = getHashInfoFromUri(uri)
+        expect(info.hash).toBeUndefined()
+      })
+    })
+    describe('type', () => {
+      const uris = ['https://beta.explore.xyo.network/archive', 'https://explore.xyo.network/?network=main']
+      it.each(uris)('returns undefined', (uri) => {
+        const info = getHashInfoFromUri(uri)
+        expect(info.type).toBeUndefined()
+      })
+    })
+  })
+})

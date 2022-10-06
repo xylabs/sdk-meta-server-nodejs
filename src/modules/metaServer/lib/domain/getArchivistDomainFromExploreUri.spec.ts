14a31fe68686c660d7e7270c840fb0292d8f9bd6
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-05 20:32:40 -0500

    Handle missing network in query params

--- src/modules/metaServer/lib/domain/getArchivistDomainFromExploreUri.spec.ts
+++ src/modules/metaServer/lib/domain/getArchivistDomainFromExploreUri.spec.ts
@@ -1,30 +1,35 @@
 import { getArchivistDomainFromExploreUri, networkToArchivistUri } from './getArchivistDomainFromExploreUri'
 
 const prodUrls = [
+  'https://explore.xyo.network',
+  'https://explore.xyo.network/',
   'https://explore.xyo.network/?network=main',
+  'https://explore.xyo.network/?debug=true&network=main',
+  'https://explore.xyo.network/archive/temp/block/hash/ce9d1723d3f501abc980c4f48248c11c9689051d9a2010d521e4ee484b260436',
   'https://beta.explore.xyo.network/?network=main',
   'http://locahost:3000/?network=main',
   'http://aws-alb-123456789.us-east-1.elb.amazonaws.com:80/archive/temp/payload/hash/eef3ce837b279bdea1688ef7cd2e0606c388984d7ae950702b276e466528b789?network=main',
 ]
 const betaUrls = [
-  'https://explore.xyo.network/?network=kerplunk',
+  'https://beta.explore.xyo.network',
+  'https://beta.explore.xyo.network/',
   'https://beta.explore.xyo.network/?network=kerplunk',
+  'https://beta.explore.xyo.network/?debug=true&network=kerplunk',
+  'https://beta.explore.xyo.network/archive/temp/block/hash/18b79aa80de2bcc7287fd0577a5c2caee47a37fd4ac75a252e4bc05aeddd7fbb',
+  'https://explore.xyo.network/?network=kerplunk',
   'http://locahost:3000/?network=kerplunk',
   'http://aws-alb-123456789.us-east-1.elb.amazonaws.com:80/archive/temp/payload/hash/eef3ce837b279bdea1688ef7cd2e0606c388984d7ae950702b276e466528b789?network=kerplunk',
 ]
 const localhostUrls = [
+  'http://localhost:3000',
+  'http://localhost:3000/',
   'https://explore.xyo.network/?network=local',
+  'https://explore.xyo.network/?debug=true&network=local',
   'https://beta.explore.xyo.network/?network=local',
   'http://locahost:3000/?network=local',
   'http://aws-alb-123456789.us-east-1.elb.amazonaws.com:80/archive/temp/payload/hash/eef3ce837b279bdea1688ef7cd2e0606c388984d7ae950702b276e466528b789?network=local',
 ]
-const invalidUris = [
-  'https://www.google.com/search/foo',
-  'https://www.google.com',
-  'https://beta.explore.xyo.network/archive/temp/block/hash/18b79aa80de2bcc7287fd0577a5c2caee47a37fd4ac75a252e4bc05aeddd7fbb',
-  'https://explore.xyo.network/archive/temp/block/hash/ce9d1723d3f501abc980c4f48248c11c9689051d9a2010d521e4ee484b260436',
-  'https://api.archivist.xyo.network',
-]
+const invalidUris = ['https://www.google.com/search/foo', 'https://www.google.com', 'https://api.archivist.xyo.network']
 
 describe('getArchivistDomainFromExploreUri', () => {
   it.each(prodUrls)('gets production domain from explorer URI', (uri) => {

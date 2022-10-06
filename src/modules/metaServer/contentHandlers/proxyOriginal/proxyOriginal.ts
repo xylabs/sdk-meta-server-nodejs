4819d4f8f9b63330a11b8623946d58fa36e31f78
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-05 19:52:09 -0500

    Serve known files or index.html

--- src/modules/metaServer/contentHandlers/proxyOriginal/proxyOriginal.ts
+++ src/modules/metaServer/contentHandlers/proxyOriginal/proxyOriginal.ts
@@ -1,8 +1,14 @@
+import { assertEx } from '@xylabs/assert'
+import { RequestHandler } from 'express'
+import { existsSync, readFileSync } from 'fs'
+import { join } from 'path'
 import serveStatic, { ServeStaticOptions } from 'serve-static'
 
+import { getAdjustedPath, isKnownFileExtension } from '../../lib'
 import { ApplicationMiddlewareOptions, MountPathAndMiddleware } from '../../types'
 
 const oneDayInMs = 1000 * 60 * 60 * 24
+
 const options: ServeStaticOptions = {
   cacheControl: true,
   // etag: true,
@@ -11,12 +17,20 @@ const options: ServeStaticOptions = {
 }
 
 const getHandler = (baseDir: string) => {
-  return serveStatic(baseDir, options)
-
-  // return asyncHandler(async (req, res) => {
-  //   const adjustedPath = getAdjustedPath(req)
-  //   res.send(await readFile(join(baseDir, adjustedPath)))
-  // })
+  // If file containing standard HTML meta exists use it otherwise use defaults
+  const filePath = join(baseDir, 'index.html')
+  assertEx(existsSync(filePath), 'Missing index.html')
+  const html = readFileSync(filePath, { encoding: 'utf-8' })
+  const proxy = serveStatic(baseDir, options)
+  const handler: RequestHandler = (req, res, next) => {
+    const adjustedPath = getAdjustedPath(req)
+    if (isKnownFileExtension(adjustedPath)) {
+      proxy(req, res, next)
+    } else {
+      res.set('Cache-Control', `public, max-age=${oneDayInMs}`).send(html)
+    }
+  }
+  return handler
 }
 
 /**

12190cacc8a2f57be0ccb3fa3798753543a4ebd3
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-06 17:31:55 -0500

    Tests for all error paths

--- src/modules/metaServer/contentHandlers/proxyOriginal/proxyOriginal.spec.ts
+++ src/modules/metaServer/contentHandlers/proxyOriginal/proxyOriginal.spec.ts
@@ -46,4 +46,25 @@ describe('proxyOriginal', () => {
       expect(actual).toBe(expected)
     })
   })
+  describe('when requested resource is not a file', () => {
+    it('serves up default index.html', async () => {
+      // Serve up this directory
+      const server = getServer(__dirname)
+      const serverRelativePath = '/test/directory.test'
+      expect(serverRelativePath).toBeTruthy()
+
+      // Get this file from the server
+      const response = await server.get(serverRelativePath).expect(200)
+      expect(response.body).toBeTruthy()
+      const actual = response.text.toString()
+      expect(actual).toBeTruthy()
+
+      // Get this file by reading it directly from the filesystem
+      const expected = await readFile(join(__dirname, 'index.html'), { encoding: 'utf-8' })
+      expect(expected).toBeTruthy()
+
+      // Compare served up version with actual for equality
+      expect(actual).toBe(expected)
+    })
+  })
 })

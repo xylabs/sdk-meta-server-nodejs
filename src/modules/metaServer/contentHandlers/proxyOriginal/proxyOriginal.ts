d0cc409a805e8e77e517311c6d950e54c66a79b9
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-06 18:12:22 -0500

    Use caching to prevent repetitive file existence checks

--- src/modules/metaServer/contentHandlers/proxyOriginal/proxyOriginal.ts
+++ src/modules/metaServer/contentHandlers/proxyOriginal/proxyOriginal.ts
@@ -1,8 +1,9 @@
 import { assertEx } from '@xylabs/assert'
 import { asyncHandler } from '@xylabs/sdk-api-express-ecs'
-import { RequestHandler } from 'express'
+import { NextFunction, Request, RequestHandler, Response } from 'express'
 import { existsSync, readFileSync } from 'fs'
 import { stat } from 'fs/promises'
+import LruCache from 'lru-cache'
 import { join } from 'path'
 import serveStatic, { ServeStaticOptions } from 'serve-static'
 
@@ -18,24 +19,35 @@ const options: ServeStaticOptions = {
   maxAge: oneDayInMs,
 }
 
+const existingFiles = new LruCache<string, boolean>({ max: 1000 })
+
 const getHandler = (baseDir: string) => {
   // Ensure file containing base HTML exists
   const filePath = join(baseDir, 'index.html')
   assertEx(existsSync(filePath), 'Missing index.html')
   const html = readFileSync(filePath, { encoding: 'utf-8' })
   const proxy = serveStatic(baseDir, options)
-  const serveIndex: RequestHandler = (_req, res, _next) => {
-    res.type('html').set('Cache-Control', `public, max-age=${oneDayInMs}`).send(html)
+  const serveIndex: RequestHandler = (_req, res, _next) => res.type('html').set('Cache-Control', `public, max-age=${oneDayInMs}`).send(html)
+  const proxyIfExists = (req: Request, res: Response, next: NextFunction, exists: boolean) => {
+    if (exists) {
+      proxy(req, res, next)
+    } else {
+      serveIndex(req, res, next)
+    }
   }
   const handler: RequestHandler = async (req, res, next) => {
     try {
       // Check if file exists on disk and proxy
-      const requestedFile = join(baseDir, getAdjustedPath(req))
-      // NOTE: Stat throws if file doesn't exist
-      if ((await stat(requestedFile)).isFile()) {
-        proxy(req, res, next)
+      const file = getAdjustedPath(req)
+      const cachedResult = existingFiles.get(file)
+      if (cachedResult !== undefined) {
+        proxyIfExists(req, res, next, cachedResult)
       } else {
-        serveIndex(req, res, next)
+        const path = join(baseDir, file)
+        // NOTE: Stat throws if file doesn't exist
+        const exists = (await stat(path)).isFile()
+        existingFiles.set(file, exists)
+        proxyIfExists(req, res, next, exists)
       }
     } catch (error) {
       serveIndex(req, res, next)

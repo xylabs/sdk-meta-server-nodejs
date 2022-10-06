08ce42d9b5064f3a4dfbbf92f101f967f143d673
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-05 19:21:13 -0500

    Use mime library to infer known file types

--- src/modules/metaServer/lib/file/isKnownFileExtension.ts
+++ src/modules/metaServer/lib/file/isKnownFileExtension.ts
@@ -1,8 +1,7 @@
-import { extname } from 'path'
-
-import { knownFileExtensions } from './knownFileExtensions'
+import mime from 'mime'
 
 export const isKnownFileExtension = (filePath: string): boolean => {
-  const extension = extname(filePath).toLowerCase()
-  return knownFileExtensions[extension] ?? false
+  // const extension = extname(filePath).toLowerCase()
+  // return knownFileExtensions[extension] ?? false
+  return mime.getType(filePath) ? true : false
 }

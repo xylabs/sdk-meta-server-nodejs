d9e9ab0e7c3fe0be1b080f31008e3de52167581b
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-02 21:54:42 -0500

    use different cache time for `index.html` over other static files

--- src/modules/metaServer/contentHandlers/proxyOriginal/proxyOriginal.ts
+++ src/modules/metaServer/contentHandlers/proxyOriginal/proxyOriginal.ts
@@ -10,13 +10,24 @@ import serveStatic, { ServeStaticOptions } from 'serve-static'
 import { getAdjustedPath } from '../../lib'
 import { ApplicationMiddlewareOptions, MountPathAndMiddleware } from '../../types'
 
-const oneHour = 60 * 60
+/**
+ * The max-age cache control header time (in seconds)
+ * to set for the index.html file
+ */
+const indexHtmlMaxAge = 60 * 10
+const indexHtmlCacheControlHeader = `public, max-age=${indexHtmlMaxAge}`
+
+/**
+ * The max-age cache control header time (in mS)
+ * to set for static files other than index.html
+ */
+const maxAge = 60 * 60 * 1000
 
 const options: ServeStaticOptions = {
   cacheControl: true,
   // etag: true,
   fallthrough: false,
-  maxAge: oneHour * 1000,
+  maxAge,
 }
 
 const existingFiles = new LruCache<string, boolean>({ max: 1000 })
@@ -27,7 +38,7 @@ const getHandler = (baseDir: string) => {
   assertEx(existsSync(filePath), 'Missing index.html')
   const html = readFileSync(filePath, { encoding: 'utf-8' })
   const proxy = serveStatic(baseDir, options)
-  const serveIndex: RequestHandler = (_req, res, _next) => res.type('html').set('Cache-Control', `public, max-age=${oneHour}`).send(html)
+  const serveIndex: RequestHandler = (_req, res, _next) => res.type('html').set('Cache-Control', indexHtmlCacheControlHeader).send(html)
   const proxyIfExists = (req: Request, res: Response, next: NextFunction, exists: boolean) => {
     if (exists) {
       proxy(req, res, next)

b960c00ebade1bd446aa614d2356f629bc473325
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-07 14:53:12 -0600

    Update documentation

--- src/modules/metaServer/lib/path/isHtmlLike.ts
+++ src/modules/metaServer/lib/path/isHtmlLike.ts
@@ -4,18 +4,16 @@ import { extname } from 'path'
 import { isKnownFileExtension } from '../file'
 
 /**
- * Return request path if the request path has an
- * extension AND the extension is know web extension
- * Otherwise, add index.html if path has no extension
- * OR is not a known web extension
+ * Returns true request path could be referring
+ * to an HTML file
  * @param req The web request
- * @returns The appropriate path for the request path
+ * @returns True if the request path could be referring,
+ * to an HTML document, false otherwise
  */
 export const isHtmlLike = (req: Request): boolean => {
   const ext = extname(req.path)
   if (ext.length === 0) return true
   if (ext.toLowerCase() === '.html') return true
   if (!isKnownFileExtension(req.path)) return true
-  // Has extension but now known web extension
   return false
 }

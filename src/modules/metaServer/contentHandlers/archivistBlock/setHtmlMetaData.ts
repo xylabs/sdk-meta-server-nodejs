7fa501f019ebd0832e6c1932d8085e10e716658d
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-16 15:50:28 -0500

    Update bw/payload metadata method
    To use new SDK

--- src/modules/metaServer/contentHandlers/archivistBlock/setHtmlMetaData.ts
+++ src/modules/metaServer/contentHandlers/archivistBlock/setHtmlMetaData.ts
@@ -1,29 +1,36 @@
-import { XyoArchivistApi } from '@xyo-network/api'
+import { assertEx } from '@xylabs/assert'
+import { ArchivistWrapper } from '@xyo-network/archivist-wrapper'
+import { XyoBoundWitnessSchema } from '@xyo-network/boundwitness-model'
+import { HttpBridge, HttpBridgeConfigSchema } from '@xyo-network/http-bridge'
 import { PayloadWrapper } from '@xyo-network/payload-wrapper'
 import { Meta, metaBuilder } from '@xyo-network/sdk-meta'
 import cloneDeep from 'lodash/cloneDeep'
 
 import { ExplorerArchivistBlockInfo } from '../../types'
 
+const schema = HttpBridgeConfigSchema
+const security = { allowAnonymous: true }
+
+const name = 'Archivist'
+
 export const setHtmlMetaData = async (info: ExplorerArchivistBlockInfo, html: string, config: Meta): Promise<string> => {
-  const { apiDomain, archive, hash, path, type } = info
+  const { apiDomain, hash, path } = info
   const meta = cloneDeep(config)
-  if (archive && hash && type && apiDomain) {
-    const api = new XyoArchivistApi({ apiDomain })
+  if (hash && apiDomain) {
     try {
-      const results = type === 'block' ? await api.archive(archive).block.hash(hash).get() : await api.archive(archive).payload.hash(hash).get()
-      if (results && results.length > 0) {
-        const wrapper = new PayloadWrapper(results[0])
-        const hash = wrapper.hash
-        const name = type === 'block' ? 'Bound Witness' : 'Payload'
-        meta.title = `XYO 2.0: ${name} | ${hash}`
-        meta.description = `A XYO 2.0 ${wrapper.body.schema} payload with the hash ${hash}.`
-      }
+      const bridge = await HttpBridge.create({ config: { nodeUri: `${apiDomain}/node`, schema, security } })
+      const resolved = await bridge.downResolver.resolve({ name: [name] })
+      const mod = assertEx(resolved.pop(), `Failed to load module [${name}]`)
+      const archivist = ArchivistWrapper.wrap(mod)
+      const results = await archivist.get([hash])
+      const wrapper = new PayloadWrapper(results[0])
+      const type = wrapper.schema === XyoBoundWitnessSchema ? 'Bound Witness' : 'Payload'
+      meta.title = `XYO 2.0: ${type} | ${hash}`
+      meta.description = `A XYO 2.0 ${wrapper.body.schema} payload with the hash ${hash}.`
     } catch (error) {
       console.log(error)
     }
   }
-
   meta.og = { ...meta.og, title: meta.title, url: path } as typeof meta.og
   meta.twitter = { ...meta.twitter, title: meta.title }
   const updated = metaBuilder(html, meta)

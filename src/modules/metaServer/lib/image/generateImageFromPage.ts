a350f1e68833bba1e4c0080076ff0e4ebbca826a
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-05 17:15:52 -0500

    Handle browser sizing

--- src/modules/metaServer/lib/image/generateImageFromPage.ts
+++ src/modules/metaServer/lib/image/generateImageFromPage.ts
@@ -1,11 +1,45 @@
-import { launch } from 'puppeteer'
+import { launch, ScreenshotOptions, Viewport } from 'puppeteer'
 
-export const generateImageFromPage = async (url: string): Promise<Buffer | undefined> => {
-  const browser = await launch(/* { headless: false, defaultViewport: null } */)
+export type ImageEncoding = 'base64' | 'binary'
+
+export interface ViewPortSize {
+  height: number
+  width: number
+}
+
+// NOTE: Should we use 16:9 ratio instead?
+// https://developers.facebook.com/docs/sharing/best-practices/
+const defaultViewportSize: ViewPortSize = {
+  height: 1080,
+  width: 1080,
+}
+
+const viewPortDefaults: Viewport = {
+  ...defaultViewportSize,
+  deviceScaleFactor: 1,
+  hasTouch: false,
+  isLandscape: false,
+  isMobile: true, // So we can render as lean as possible
+}
+
+export const generateImageFromPage = async (
+  url: string,
+  encoding: ImageEncoding = 'base64',
+  viewportSize: ViewPortSize = defaultViewportSize,
+  outputPath?: string,
+): Promise<Buffer | string | undefined> => {
+  const defaultViewport = { ...viewPortDefaults, ...viewportSize }
+  const browser = await launch({ defaultViewport, headless: 'new' })
   try {
     const [page] = await browser.pages()
     await page.goto(url)
-    const image = await page.screenshot({ fullPage: true, path: 'screenshot.png', type: 'png' })
+    const opts: ScreenshotOptions = {
+      encoding,
+      fullPage: true,
+      type: 'png',
+    }
+    if (outputPath) opts.path = outputPath
+    const image = await page.screenshot(opts)
     return image
   } catch (err) {
     console.error(err)
--- src/modules/metaServer/lib/image/spec/generateImageFromPage.spec.ts
+++ src/modules/metaServer/lib/image/spec/generateImageFromPage.spec.ts
@@ -2,7 +2,7 @@ import { generateImageFromPage } from '../generateImageFromPage'
 
 describe('generateImageFromPage', () => {
   it('generates image from page', async () => {
-    const image = await generateImageFromPage('https://xyo.network')
+    const image = await generateImageFromPage('https://dataism.org', 'binary', { height: 1080, width: 1080 }, './test.png')
     expect(image).toBeDefined()
   })
 })

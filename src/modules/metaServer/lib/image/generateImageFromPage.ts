59e737cc6d4c0a964c4952bed2e2a70b3e9611d6
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-06 20:45:07 -0500

    Typed page image generators

--- src/modules/metaServer/lib/image/generateImageFromPage.ts
+++ src/modules/metaServer/lib/image/generateImageFromPage.ts
@@ -10,8 +10,18 @@ export type PageImageOptions = PageRenderingOptions & {
 }
 
 export const defaultScreenshotOptions: ScreenshotOptions = {
-  encoding: 'binary',
+  encoding: 'base64',
   type: 'png',
+}
+
+export const generateImageStringFromPage = (page: Page, screenshotOptions: ScreenshotOptions = defaultScreenshotOptions): Promise<string> => {
+  const options = { ...screenshotOptions, encoding: 'base64' } as const
+  return page.screenshot(options)
+}
+
+export const generateImageBufferFromPage = (page: Page, screenshotOptions: ScreenshotOptions = defaultScreenshotOptions): Promise<Buffer> => {
+  const options = { ...screenshotOptions, encoding: 'binary' } as const
+  return page.screenshot(options)
 }
 
 export const generateImageFromPage = (

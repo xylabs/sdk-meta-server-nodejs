e79b031f444fa6bba5d1131f5e6384eae925f73e
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-06 20:57:45 -0500

    Tests for augmented head

--- src/modules/metaServer/contentHandlers/foreventory/lib/spec/usePageMetaWithImage.spec.ts
+++ src/modules/metaServer/contentHandlers/foreventory/lib/spec/usePageMetaWithImage.spec.ts
@@ -2,7 +2,7 @@ import { usePageMetaWithImage } from '../usePageMetaWithImage'
 
 describe('usePageMetaWithImage', () => {
   describe('for Payload URLs sets standard meta fields', () => {
-    const cases: string[] = ['']
+    const cases: string[] = ['https://dataism.org']
     it.each(cases)('sets fields based on the  %s', async (path) => {
       const newHtml = await usePageMetaWithImage(path)
       expect(newHtml).toBeString()

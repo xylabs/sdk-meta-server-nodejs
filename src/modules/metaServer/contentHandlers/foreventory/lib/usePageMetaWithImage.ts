a301afd5c482d8ba118224f284ed61421da254c5
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-06 20:55:46 -0500

    Update HTML Meta using page scrape

--- src/modules/metaServer/contentHandlers/foreventory/lib/usePageMetaWithImage.ts
+++ src/modules/metaServer/contentHandlers/foreventory/lib/usePageMetaWithImage.ts
@@ -1,7 +1,28 @@
-export const usePageMetaWithImage = async (_path: string): Promise<string | undefined> => {
+import { Meta, metaBuilder } from '@xyo-network/sdk-meta'
+
+import { generateImageStringFromPage, usePage } from '../../../lib'
+
+export const usePageMetaWithImage = async (url: string): Promise<string | undefined> => {
   try {
-    // TODO: Get from puppeteer
-    await Promise.resolve()
+    const updatedHtml = await usePage(url, undefined, async (page) => {
+      const image = await generateImageStringFromPage(page)
+      const html = await page.content()
+      const meta: Meta = {
+        og: {
+          image: {
+            type: 'image/png;base64',
+            url: image,
+          },
+        },
+        twitter: {
+          image: {
+            url: image,
+          },
+        },
+      }
+      return metaBuilder(html, meta)
+    })
+    return updatedHtml
   } catch (error) {
     console.log(error)
   }

c1f4285f49ac775647c3dfe55f766508a85f96e2
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-07 20:14:38 -0500

    Cleanup image meta generation

--- src/modules/metaServer/contentHandlers/foreventory/lib/usePageMetaWithImage.ts
+++ src/modules/metaServer/contentHandlers/foreventory/lib/usePageMetaWithImage.ts
@@ -1,4 +1,4 @@
-import { Meta, metaBuilder } from '@xyo-network/sdk-meta'
+import { Meta, metaBuilder, OpenGraphMeta, TwitterMeta } from '@xyo-network/sdk-meta'
 import { URL } from 'url'
 
 import { summaryCardWithLargeImageFromPage, usePage } from '../../../lib'
@@ -12,25 +12,13 @@ const getImageUrl = (url: string): string => {
 export const usePageMetaWithImage = async (url: string, imageCache: ImageCache): Promise<string | undefined> => {
   try {
     const updatedHtml = await usePage(url, undefined, async (page) => {
+      const html = await page.content()
       const image = await summaryCardWithLargeImageFromPage(page)
       const imageUrl = getImageUrl(url)
       imageCache.set(imageUrl, image)
-      const html = await page.content()
-      const meta: Meta = {
-        og: {
-          image: {
-            '': imageUrl,
-            secure_url: imageUrl,
-            type: 'image/png',
-            url: imageUrl,
-          },
-        },
-        twitter: {
-          image: {
-            '': imageUrl,
-          },
-        },
-      }
+      const og: OpenGraphMeta = { image: { '': imageUrl, secure_url: imageUrl, type: 'image/png', url: imageUrl } }
+      const twitter: TwitterMeta = { image: { '': imageUrl } }
+      const meta: Meta = { og, twitter }
       return metaBuilder(html, meta)
     })
     return updatedHtml

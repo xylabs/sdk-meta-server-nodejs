1508cdcd4d9b492cde6f65abcc6f8b7f6dc84cb0
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-07 19:42:23 -0500

    Remove duplicate method

--- src/modules/metaServer/lib/page/index.ts
+++ src/modules/metaServer/lib/page/index.ts
@@ -1,4 +1,3 @@
-export * from './pageHtmlWithPageImageAsMeta'
 export * from './PageRenderingOptions'
 export * from './usePage'
 export * from './ViewPortSize'
--- src/modules/metaServer/lib/page/pageHtmlWithPageImageAsMeta.ts
+++
@@ -1,9 +0,0 @@
-import { usePage } from './usePage'
-
-export const pageHtmlWithPageImageAsMeta = async (url: string): Promise<string | undefined> => {
-  const html: string | undefined = undefined
-  await usePage(url, undefined, (page) => {
-    // TODO: Generate image from page
-  })
-  return html
-}

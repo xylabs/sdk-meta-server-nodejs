5676f2d99ade6b06dd919abecad68475a69c4b1b
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-10 08:19:19 -0500

    Update tests

--- src/modules/metaServer/lib/head/spec/getRenderedHead.spec.ts
+++ src/modules/metaServer/lib/head/spec/getRenderedHead.spec.ts
@@ -1,17 +1,24 @@
 import { usePage } from '../../page'
-import { getRenderedHead, getRenderedMeta } from '../getRenderedHead'
+import { getRenderedHead, getRenderedMeta, getRenderedMetaElements } from '../getRenderedHead'
 
 const uris = ['https://www.youtube.com/watch?v=Kauv7MVPcsA']
 
 describe('getRenderedHead', () => {
-  it.each(uris)('Returns true for known file extensions', async (uri) => {
+  it.each(uris)('Returns element from page', async (uri) => {
     const head = await usePage(uri, undefined, (page) => getRenderedHead(page))
     expect(head).toBeString()
   })
 })
 
+describe('getRenderedMetaElements', () => {
+  it.each(uris)('Returns elements from page', async (uri) => {
+    const meta = await usePage(uri, undefined, (page) => getRenderedMetaElements(page))
+    expect(meta).toBeArray()
+  })
+})
+
 describe('getRenderedMeta', () => {
-  it.each(uris)('Returns true for known file extensions', async (uri) => {
+  it.each(uris)('Returns object from elements on page', async (uri) => {
     const head = await usePage(uri, undefined, (page) => getRenderedMeta(page))
     expect(head).toBeObject()
   })

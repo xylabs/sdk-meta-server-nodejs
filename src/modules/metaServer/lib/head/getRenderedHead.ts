2848514daf3582396addda38371b723c718fde43
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-10 08:17:38 -0500

    remove unused code

--- src/modules/metaServer/lib/head/getRenderedHead.ts
+++ src/modules/metaServer/lib/head/getRenderedHead.ts
@@ -3,32 +3,20 @@ import { Page } from 'puppeteer'
 
 export const getRenderedHead = async (page: Page) => {
   const renderedHeadElement = await page.$('head')
-  const renderedMetaElement = await renderedHeadElement?.$('meta')
-  const renderedMetaElements = await page.$$('head>meta')
-  const renderedTitleElement = await renderedHeadElement?.$('title')
-  const renderedDescriptionElement = await renderedHeadElement?.$('description')
+  return await renderedHeadElement?.evaluate((head) => head.innerHTML)
+}
 
-  const head = await renderedHeadElement?.evaluate((head) => head.innerHTML)
-  const headOut = await renderedHeadElement?.evaluate((head) => head.outerHTML)
-  const meta = await renderedMetaElement?.evaluate((meta) => meta.outerHTML)
-  const metas = await Promise.all(renderedMetaElements?.map((e) => e?.evaluate((meta) => meta.outerHTML)))
-  const title = await renderedTitleElement?.evaluate((title) => title.innerHTML)
-  const description = await renderedDescriptionElement?.evaluate((description) => description.innerHTML)
-  return ''
+export const getRenderedMetaElements = async (page: Page) => {
+  const renderedMetaElements = await page.$$('head>meta')
+  return await Promise.all(renderedMetaElements?.map((e) => e?.evaluate((meta) => meta.outerHTML)))
 }
 
 export const getRenderedMeta = async (page: Page): Promise<Meta> => {
   const ret: Meta = {}
   const renderedHeadElement = await page.$('head')
-  const renderedMetaElement = await renderedHeadElement?.$('meta')
-  const renderedMetaElements = await page.$$('head>meta')
   const renderedTitleElement = await renderedHeadElement?.$('title')
   const renderedDescriptionElement = await renderedHeadElement?.$('meta[name="description"]')
-
-  const head = await renderedHeadElement?.evaluate((head) => head.innerHTML)
-  const headOut = await renderedHeadElement?.evaluate((head) => head.outerHTML)
-  const meta = await renderedMetaElement?.evaluate((meta) => meta.outerHTML)
-  const metas = await Promise.all(renderedMetaElements?.map((e) => e?.evaluate((meta) => meta.outerHTML)))
+  // Get more meta tags here (og, twitter, etc.)
   const title = await renderedTitleElement?.evaluate((title) => title.innerHTML)
   const description = await renderedDescriptionElement?.evaluate((description) => description.getAttribute('content') ?? undefined)
   ret.title = title
--- src/modules/metaServer/lib/head/spec/getRenderedHead.spec.ts
+++ src/modules/metaServer/lib/head/spec/getRenderedHead.spec.ts
@@ -5,20 +5,14 @@ const uris = ['https://www.youtube.com/watch?v=Kauv7MVPcsA']
 
 describe('getRenderedHead', () => {
   it.each(uris)('Returns true for known file extensions', async (uri) => {
-    const head = await usePage(uri, undefined, async (page) => {
-      expect(await getRenderedHead(page)).toBe(true)
-      return getRenderedHead(page)
-    })
+    const head = await usePage(uri, undefined, (page) => getRenderedHead(page))
     expect(head).toBeString()
   })
 })
 
 describe('getRenderedMeta', () => {
   it.each(uris)('Returns true for known file extensions', async (uri) => {
-    const head = await usePage(uri, undefined, async (page) => {
-      expect(await getRenderedMeta(page)).toBe(true)
-      return getRenderedMeta(page)
-    })
+    const head = await usePage(uri, undefined, (page) => getRenderedMeta(page))
     expect(head).toBeObject()
   })
 })

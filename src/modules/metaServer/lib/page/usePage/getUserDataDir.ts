6627312f81153a7d1804ea0772069999e524800e
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-12 14:12:01 -0500

    Documentation

--- src/modules/metaServer/lib/page/usePage/getUserDataDir.ts
+++ src/modules/metaServer/lib/page/usePage/getUserDataDir.ts
@@ -4,8 +4,8 @@ import { join } from 'path'
 const root = './puppeteer/cache'
 
 /**
- * Ensures we don't bloat the cache with too many entries
- * by creating a new one every hour
+ * Ensures we don't bloat the cache with too many entries and
+ * slowing down rendering by creating a new one every hour
  * @returns User data dir including ./puppeteer/cache/2023-07-12-14
  */
-export const getUserDataDir = () => join(root, format(new Date(2014, 1, 11), 'yyyy-MM-dd-HH'))
+export const getUserDataDir = () => join(root, format(new Date(), 'yyyy-MM-dd-HH'))

b548fa199dae6815f346984b4c209a592dd30341
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-11 23:21:36 -0500

    Skip lib function tests

--- src/modules/metaServer/lib/head/spec/getRenderedHead.spec.ts
+++ src/modules/metaServer/lib/head/spec/getRenderedHead.spec.ts
@@ -3,29 +3,31 @@ import { getRenderedHead, getRenderedMeta, getRenderedMetaElements, getSelective
 
 const uris = ['https://www.youtube.com/watch?v=Kauv7MVPcsA']
 
-describe('getRenderedHead', () => {
-  it.each(uris)('Returns element from page', async (uri) => {
-    const head = await usePage(uri, undefined, (page) => getRenderedHead(page))
-    expect(head).toBeString()
+describe.skip('getRenderedHead', () => {
+  describe('getRenderedHead', () => {
+    it.each(uris)('Returns element from page', async (uri) => {
+      const head = await usePage(uri, undefined, (page) => getRenderedHead(page))
+      expect(head).toBeString()
+    })
   })
-})
 
-describe('getRenderedMetaElements', () => {
-  it.each(uris)('Returns elements from page', async (uri) => {
-    const meta = await usePage(uri, undefined, (page) => getRenderedMetaElements(page))
-    expect(meta).toBeArray()
+  describe('getRenderedMetaElements', () => {
+    it.each(uris)('Returns elements from page', async (uri) => {
+      const meta = await usePage(uri, undefined, (page) => getRenderedMetaElements(page))
+      expect(meta).toBeArray()
+    })
   })
-})
 
-describe('getRenderedMeta', () => {
-  it.each(uris)('Returns object from elements on page', async (uri) => {
-    const head = await usePage(uri, undefined, (page) => getRenderedMeta(page))
-    expect(head).toBeObject()
+  describe('getRenderedMeta', () => {
+    it.each(uris)('Returns object from elements on page', async (uri) => {
+      const head = await usePage(uri, undefined, (page) => getRenderedMeta(page))
+      expect(head).toBeObject()
+    })
   })
-})
-describe.only('getSelectiveHeadElements', () => {
-  it.each(uris)('Returns object from elements on page', async (uri) => {
-    const elements = await usePage(uri, undefined, (page) => getSelectiveHeadElements(page))
-    expect(elements).toBeArray()
+  describe('getSelectiveHeadElements', () => {
+    it.each(uris)('Returns object from elements on page', async (uri) => {
+      const elements = await usePage(uri, undefined, (page) => getSelectiveHeadElements(page))
+      expect(elements).toBeArray()
+    })
   })
 })

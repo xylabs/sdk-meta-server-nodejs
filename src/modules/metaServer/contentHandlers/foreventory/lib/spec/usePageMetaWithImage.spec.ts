1028b01e7ac92828787b9b0ae9a040d15e50764d
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-12 01:21:18 -0500

    Use image url helper for tests

--- src/modules/metaServer/contentHandlers/foreventory/lib/spec/usePageMetaWithImage.spec.ts
+++ src/modules/metaServer/contentHandlers/foreventory/lib/spec/usePageMetaWithImage.spec.ts
@@ -1,5 +1,6 @@
 import { mock } from 'jest-mock-extended'
 
+import { getImageUrl } from '../getImageUrl'
 import { ImageCache } from '../imageCache'
 import { usePageMetaWithImage } from '../usePageMetaWithImage'
 
@@ -10,17 +11,18 @@ describe('usePageMetaWithImage', () => {
       const hash = '27c9343b576c59c40410e98226b98749b859abfa664054cd2fb2e7699cdf9f23'
       const width = 1600
       const height = 900
-      const url = `https://insiders.foreventory.com/netflix/insights/${hash}`
+      const url = `${domain}/netflix/insights/${hash}`
+      const imageUrl = getImageUrl(`${url}/preview`, width, height)
       const imageCache = mock<ImageCache>()
       const newHtml = await usePageMetaWithImage(url, imageCache)
       expect(newHtml).toBeString()
-      expect(newHtml).toContain(`<meta property="og:image" content="${domain}/netflix/insights/${hash}/preview/${width}/${height}">`)
+      expect(newHtml).toContain(`<meta property="og:image" content="${imageUrl}">`)
       expect(newHtml).toContain(`<meta property="og:image:height" content="${height}">`)
-      expect(newHtml).toContain(`<meta property="og:image:secure_url" content="${domain}/netflix/insights/${hash}/preview/${width}/${height}">`)
+      expect(newHtml).toContain(`<meta property="og:image:secure_url" content="${imageUrl}">`)
       expect(newHtml).toContain('<meta property="og:image:type" content="image/png">')
-      expect(newHtml).toContain(`<meta property="og:image:url" content="${domain}/netflix/insights/${hash}/preview/${width}/${height}">`)
+      expect(newHtml).toContain(`<meta property="og:image:url" content="${imageUrl}">`)
       expect(newHtml).toContain(`<meta property="og:image:width" content="${width}">`)
-      expect(newHtml).toContain(`<meta property="twitter:image" content="${domain}/netflix/insights/${hash}/preview/${width}/${height}">`)
+      expect(newHtml).toContain(`<meta property="twitter:image" content="${imageUrl}">`)
     })
   })
 })

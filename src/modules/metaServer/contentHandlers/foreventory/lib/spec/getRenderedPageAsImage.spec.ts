63dcd908f357f216d78ec12c61c920b7003d7371
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-12 12:25:34 -0500

    Perf tests

---
+++ src/modules/metaServer/contentHandlers/foreventory/lib/spec/getRenderedPageAsImage.spec.ts
@@ -0,0 +1,33 @@
+import { mock } from 'jest-mock-extended'
+
+import { ImageCache } from '../cache'
+import { getRenderedPageAsImage } from '../getRenderedPageAsImage'
+
+describe('getRenderedPageAsImage', () => {
+  let average = 0
+  let duration = 0
+  beforeAll(() => {
+    jest.spyOn(console, 'log').mockImplementation(() => {
+      // Stop expected logs from being generated during tests
+    })
+    jest.spyOn(console, 'warn').mockImplementation(() => {
+      // Stop expected logs from being generated during tests
+    })
+  })
+  afterAll(() => {
+    jest.clearAllMocks()
+    console.error(`Duration: ${duration}ms`)
+    console.error(`Average: ${average}ms`)
+  })
+  it('perf test', async () => {
+    const imageCache = mock<ImageCache>()
+    const url = 'https://www.google.com'
+    const start = Date.now()
+    const iterations = 10
+    for (let i = 0; i < iterations; i++) {
+      await getRenderedPageAsImage(url, imageCache)
+    }
+    duration = Date.now() - start
+    average = duration / iterations
+  }, 60000)
+})

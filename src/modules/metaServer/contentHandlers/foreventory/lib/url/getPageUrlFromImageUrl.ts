8d6d3dfa2fc4285c2220aaeacc4c043a5174bde3
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-13 22:35:23 -0500

    Use regex to reverse image to preview

--- src/modules/metaServer/contentHandlers/foreventory/lib/url/getPageUrlFromImageUrl.ts
+++ src/modules/metaServer/contentHandlers/foreventory/lib/url/getPageUrlFromImageUrl.ts
@@ -1,3 +1,7 @@
+// Create a regular expression that matches a SHA-256 hash and everything after it
+const regex = new RegExp('([a-fA-F0-9]{64}).*')
+
 export const getPageUrlFromImageUrl = (url: string): string => {
-  return url.replace('/img.png', '')
+  // Replace the matched substring with the hash itself
+  return url.replace(regex, '$1')
 }
--- src/modules/metaServer/contentHandlers/foreventory/lib/url/spec/getPageUrlFromImageUrl.spec.ts
+++ src/modules/metaServer/contentHandlers/foreventory/lib/url/spec/getPageUrlFromImageUrl.spec.ts
@@ -1,7 +1,16 @@
 import { getPageUrlFromImageUrl } from '../getPageUrlFromImageUrl'
 
 describe('getPageUrlFromImageUrl', () => {
-  const cases: [input: string, expected: string][] = [['', '']]
+  const cases: [input: string, expected: string][] = [
+    [
+      'http://localhost:3000/netflix/insights/27c9343b576c59c40410e98226b98749b859abfa664054cd2fb2e7699cdf9f23/preview/1600/900/img.png',
+      'http://localhost:3000/netflix/insights/27c9343b576c59c40410e98226b98749b859abfa664054cd2fb2e7699cdf9f23',
+    ],
+    [
+      'https://beta.foreventory.com/netflix/insights/1f440d46ac56a44083a181728b78c004ba8063ea027860f8edf125a1d4c2a8a3/preview/1600/900/img.png',
+      'https://beta.foreventory.com/netflix/insights/1f440d46ac56a44083a181728b78c004ba8063ea027860f8edf125a1d4c2a8a3',
+    ],
+  ]
   it.each(cases)('gets page url from image url', (input, expected) => {
     expect(getPageUrlFromImageUrl(input)).toBe(expected)
   })

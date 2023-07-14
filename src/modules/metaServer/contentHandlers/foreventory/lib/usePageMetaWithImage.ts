c6f71a4517aa7c9164caa511ca94d16f09649cee
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-13 19:27:08 -0500

    Modularize rendered html strategy

--- src/modules/metaServer/contentHandlers/foreventory/lib/usePageMetaWithImage.ts
+++ src/modules/metaServer/contentHandlers/foreventory/lib/usePageMetaWithImage.ts
@@ -3,12 +3,12 @@ import { metaBuilder } from '@xyo-network/sdk-meta'
 import { join } from '../../../lib'
 import { ImageCache } from './cache'
 import { getRenderedPageAsImage } from './getRenderedPageAsImage'
-import { getRenderedHtml } from './pageStrategies/getRenderedHtml/getRenderedHtml'
+import { getRenderedPageHtml } from './pageStrategies'
 
 export const usePageMetaWithImage = async (url: string, imageCache: ImageCache): Promise<string | undefined> => {
   try {
     const previewUrl = join(url, 'preview')
-    const [html, meta] = await Promise.all([getRenderedHtml(url), getRenderedPageAsImage(previewUrl, imageCache)])
+    const [html, meta] = await Promise.all([getRenderedPageHtml(url), getRenderedPageAsImage(previewUrl, imageCache)])
     if (html && meta) return metaBuilder(html, meta)
   } catch (error) {
     console.log(error)

53667b2a606ecc11c5024571622c1c1b71639822
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-14 18:13:23 -0500

    Get payload meta from query

--- src/modules/metaServer/contentHandlers/foreventory/lib/middleware/metaQueryHandler.ts
+++ src/modules/metaServer/contentHandlers/foreventory/lib/middleware/metaQueryHandler.ts
@@ -1,11 +1,24 @@
 import { asyncHandler, Empty, NoReqParams } from '@xylabs/sdk-api-express-ecs'
+import { Payload } from '@xyo-network/payload-model'
+import { Meta } from '@xyo-network/sdk-meta'
 
-type MetaLocals = Record<'meta', string>
+import { getArchivistDomainFromUri, getArchivistForDomain, getUriBehindProxy } from '../../../../lib'
 
-export const metaQueryHandler = asyncHandler<NoReqParams, Empty, Empty, { meta?: string }, MetaLocals>((req, res, next) => {
+type MetaLocals = Record<'meta', Meta>
+
+export const metaQueryHandler = asyncHandler<NoReqParams, Empty, Empty, { meta?: string }, MetaLocals>(async (req, res, next) => {
   if (req.query.meta) {
     const hash = req.query.meta
-    // TODO: Get hash from archivist
+    const uri = getUriBehindProxy(req)
+    const domain = getArchivistDomainFromUri(uri)
+    const archivist = await getArchivistForDomain(domain)
+    const results = await archivist.get([hash])
+    const payload = results?.[0]
+    if (payload) {
+      delete (payload as Partial<Payload>)?.schema
+      const meta: Meta = { ...payload } as Meta
+      res.locals.meta = meta
+    }
   }
   next()
 })

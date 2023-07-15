9dc2f8f4a580f20d51e127e76029ff6c521ef81d
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-15 13:47:15 -0500

    Tests for SimpleMetaCache

--- src/modules/metaServer/middleware/metaCache/spec/SimpleMetaCache.spec.ts
+++ src/modules/metaServer/middleware/metaCache/spec/SimpleMetaCache.spec.ts
@@ -1,18 +1,56 @@
-describe('LruMetaCache', () => {
-  describe('get', () => {
-    it.skip('should get a value from the cache', () => {
-      // TODO: Implement
-    })
-    it.skip('return undefined if missing', () => {
-      // TODO: Implement
-    })
+import { SimpleMetaCache } from '../SimpleMetaCache'
+
+describe('SimpleMetaCache', () => {
+  let cache: SimpleMetaCache
+
+  beforeEach(() => {
+    cache = new SimpleMetaCache()
   })
-  describe('set', () => {
-    it.skip('set value if not existing', () => {
-      // TODO: Implement
-    })
-    it.skip('overwrite existing value', () => {
-      // TODO: Implement
-    })
+
+  test('set and get', () => {
+    cache.set('key1', 'value1')
+    expect(cache.get('key1')).toBe('value1')
+
+    // Edge case: key doesn't exist
+    expect(cache.get('key2')).toBeUndefined()
+  })
+
+  test('patch', () => {
+    // Trying to patch key that doesn't exist should do nothing
+    cache.patch('key1', 'value1')
+    expect(cache.get('key1')).toBeUndefined()
+
+    // Set key1
+    cache.set('key1', 'initialValue')
+    // Patch existing key
+    cache.patch('key1', 'patchedValue')
+    expect(cache.get('key1')).toBe('patchedValue')
+  })
+
+  test('entries', () => {
+    cache.set('key1', 'value1')
+    cache.set('key2', 'value2')
+
+    const entries = cache.entries()
+    expect(entries).toContainEqual(['key1', 'value1'])
+    expect(entries).toContainEqual(['key2', 'value2'])
+  })
+
+  test('keys', () => {
+    cache.set('key1', 'value1')
+    cache.set('key2', 'value2')
+
+    const keys = cache.keys()
+    expect(keys).toContain('key1')
+    expect(keys).toContain('key2')
+  })
+
+  test('values', () => {
+    cache.set('key1', 'value1')
+    cache.set('key2', 'value2')
+
+    const values = cache.values()
+    expect(values).toContain('value1')
+    expect(values).toContain('value2')
   })
 })

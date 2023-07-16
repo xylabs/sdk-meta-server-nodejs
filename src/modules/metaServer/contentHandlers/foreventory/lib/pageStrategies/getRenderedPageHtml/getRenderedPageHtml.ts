dd6c00d2522b56dcbc458834a6a1db09582f25f6
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-15 22:01:38 -0500

    documentation

--- src/modules/metaServer/contentHandlers/foreventory/lib/pageStrategies/getRenderedPageHtml/getRenderedPageHtml.ts
+++ src/modules/metaServer/contentHandlers/foreventory/lib/pageStrategies/getRenderedPageHtml/getRenderedPageHtml.ts
@@ -2,6 +2,12 @@ import { PageRenderingOptions, usePage } from '../../../../../lib'
 
 const opts: PageRenderingOptions = { waitUntil: ['domcontentloaded', 'networkidle0'] }
 
+/**
+ * Gets the rendered page html
+ * @param url The url to navigate to
+ * @param navigateToRootFirst Should navigate to the root of the url first, then navigate to the relative path
+ * @returns The rendered page html
+ */
 export const getRenderedPageHtml = async (url: string, navigateToRootFirst = false): Promise<string | undefined> => {
   try {
     console.log(`[foreventory][getRenderedPageHtml][${url}]: rendering`)

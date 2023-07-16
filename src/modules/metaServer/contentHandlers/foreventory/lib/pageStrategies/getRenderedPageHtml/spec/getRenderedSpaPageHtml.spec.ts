ceafd90fc34b0bfa15497edba1714f3358830ca3
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-16 11:20:29 -0500

    update test title

--- src/modules/metaServer/contentHandlers/foreventory/lib/pageStrategies/getRenderedPageHtml/spec/getRenderedSpaPageHtml.spec.ts
+++ src/modules/metaServer/contentHandlers/foreventory/lib/pageStrategies/getRenderedPageHtml/spec/getRenderedSpaPageHtml.spec.ts
@@ -1,6 +1,6 @@
 import { getRenderedSpaPageHtml } from '../getRenderedSpaPageHtml'
 
-describe('getRenderedPageHtml', () => {
+describe('getRenderedSpaPageHtml', () => {
   const uri = 'https://xyo.network/brand'
   const expected = '<title>XYO: Brand Assets &amp; Logos</title>'
   it('gets the page', async () => {

a17baff038a657536ba456d822be7aef7bca0536
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-16 10:56:24 -0500

    Use SPA Page helper

--- src/modules/metaServer/contentHandlers/foreventory/lib/pageStrategies/getRenderedPageHtml/getRenderedPageHtml.ts
+++ src/modules/metaServer/contentHandlers/foreventory/lib/pageStrategies/getRenderedPageHtml/getRenderedPageHtml.ts
@@ -1,6 +1,4 @@
-import { PageRenderingOptions, usePage } from '../../../../../lib'
-
-const opts: PageRenderingOptions = { waitUntil: ['domcontentloaded', 'networkidle0'] }
+import { useSpaPage } from '../../../../../lib'
 
 /**
  * Gets the rendered page html
@@ -8,21 +6,12 @@ const opts: PageRenderingOptions = { waitUntil: ['domcontentloaded', 'networkidl
  * @param navigateToRootFirst Should navigate to the root of the url first, then navigate to the relative path
  * @returns The rendered page html
  */
-export const getRenderedPageHtml = async (url: string, navigateToRootFirst = false): Promise<string | undefined> => {
+export const getRenderedPageHtml = async (url: string): Promise<string | undefined> => {
   try {
     console.log(`[foreventory][getRenderedPageHtml][${url}]: rendering`)
-    const parsed = new URL(url)
-    const urlToNavigateTo = navigateToRootFirst ? parsed.origin : url
-    const relativeUrl = parsed.pathname
-    const html = await usePage(urlToNavigateTo, opts, async (page) => {
-      console.log(`[foreventory][getRenderedPageHtml][${url}]: navigated to ${urlToNavigateTo}`)
-      if (navigateToRootFirst) {
-        await page.evaluate((path) => window.history.pushState(null, '', path), relativeUrl)
-        console.log(`[foreventory][getRenderedPageHtml][${url}]: navigated browser history to ${relativeUrl}`)
-      }
-      await page.waitForNavigation(opts)
-      const content = await page.content()
-      return content
+    const html = await useSpaPage(url, async (page) => {
+      console.log(`[foreventory][getRenderedPageHtml][${url}]: navigated to ${url}`)
+      return await page.content()
     })
     console.log(`[foreventory][getRenderedPageHtml][${url}]: rendered`)
     return html
--- src/modules/metaServer/contentHandlers/foreventory/lib/pageStrategies/getRenderedPageHtml/spec/getRenderedPageHtml.spec.ts
+++ src/modules/metaServer/contentHandlers/foreventory/lib/pageStrategies/getRenderedPageHtml/spec/getRenderedPageHtml.spec.ts
@@ -3,16 +3,8 @@ import { getRenderedPageHtml } from '../getRenderedPageHtml'
 describe('getRenderedPageHtml', () => {
   const uri = 'https://xyo.network/brand'
   const expected = '<title>XYO: Brand Assets &amp; Logos</title>'
-  describe('with navigateToRootFirst=false', () => {
-    it('gets the page', async () => {
-      const result = await getRenderedPageHtml(uri, false)
-      expect(result).toContain(expected)
-    })
-  })
-  describe('with navigateToRootFirst=true', () => {
-    it('gets the page', async () => {
-      const result = await getRenderedPageHtml(uri, true)
-      expect(result).toContain(expected)
-    })
+  it('gets the page', async () => {
+    const result = await getRenderedPageHtml(uri)
+    expect(result).toContain(expected)
   })
 })

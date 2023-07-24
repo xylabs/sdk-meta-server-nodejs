5caf2a49fcc1dd4af2ea40c6fd02db7d6f236b80
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-24 17:15:38 -0500

    Naming

--- src/modules/metaServer/lib/socialMedia/facebook/auth/spec/tryGetAppAccessToken.spec.ts
+++ src/modules/metaServer/lib/socialMedia/facebook/auth/spec/tryGetAppAccessToken.spec.ts
@@ -5,8 +5,8 @@ import { tryGetAppAccessToken } from '../tryGetAppAccessToken'
 
 describeIf(canGetAppAccessToken())('tryGetAppAccessToken', () => {
   it('Returns access_token', async () => {
-    const response = await tryGetAppAccessToken()
-    expect(response).toBeDefined()
-    expect(response).toBeString()
+    const token = await tryGetAppAccessToken()
+    expect(token).toBeDefined()
+    expect(token).toBeString()
   })
 })

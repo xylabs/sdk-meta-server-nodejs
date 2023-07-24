f6b5b5035c5f6a526c50df1a22e6d05aaf9f64ea
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-24 13:20:46 -0500

    Remove async in favor of promise

--- src/modules/metaServer/contentHandlers/foreventory/lib/image/getRenderedPageAsImage.ts
+++ src/modules/metaServer/contentHandlers/foreventory/lib/image/getRenderedPageAsImage.ts
@@ -19,22 +19,20 @@ const useLargeImage = true
 const { height, width } = useLargeImage ? summaryCardWithLargeImageViewport : summaryCardViewport
 const twitterCardGenerator = useLargeImage ? summaryCardWithLargeImageFromPage : summaryCardImageFromPage
 
-export const getRenderedPageAsImage = async (url: string, imageCache: ImageCache): Promise<Meta | undefined> => {
+export const getRenderedPageAsImage = (url: string, imageCache: ImageCache): Meta | undefined => {
   try {
-    console.log(`[foreventory][getRenderedPageAsImage][${url}]: generating image`)
-    const meta = await usePage(url, undefined, async (page) => {
-      const image = await twitterCardGenerator(page)
-      console.log(`[foreventory][getRenderedPageAsImage][${url}]: generating image url`)
-      const imageUrl = getImageUrl(url, width, height)
+    console.log(`[foreventory][getRenderedPageAsImage][${url}]: generating image url`)
+    const imageUrl = getImageUrl(url, width, height)
+    console.log(`[foreventory][getRenderedPageAsImage][${url}]: generating image meta`)
+    const og: OpenGraphMeta = { image: { '': imageUrl, height, secure_url: imageUrl, type: 'image/png', url: imageUrl, width } }
+    const twitter: TwitterMeta = { card: 'summary_large_image', image: { '': imageUrl } }
+    const meta: Meta = { og, twitter }
+    void usePage(url, undefined, (page) => {
+      console.log(`[foreventory][getRenderedPageAsImage][${url}]: generating image`)
+      imageCache.set(imageUrl, twitterCardGenerator(page))
       console.log(`[foreventory][getRenderedPageAsImage][${url}]: caching image`)
-      imageCache.set(imageUrl, image)
-      console.log(`[foreventory][getRenderedPageAsImage][${url}]: generating image meta`)
-      const og: OpenGraphMeta = { image: { '': imageUrl, height, secure_url: imageUrl, type: 'image/png', url: imageUrl, width } }
-      const twitter: TwitterMeta = { card: 'summary_large_image', image: { '': imageUrl } }
-      const meta: Meta = { og, twitter }
-      console.log(`[foreventory][getRenderedPageAsImage][${url}]: returning image meta`)
-      return meta
     })
+    console.log(`[foreventory][getRenderedPageAsImage][${url}]: returning image meta`)
     return meta
   } catch (error) {
     console.log(error)

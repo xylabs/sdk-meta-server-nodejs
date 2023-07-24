f6c64c2686f30797cf7658c9704bbe167f1c3498
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-24 16:52:34 -0500

    Add interface for response type

--- src/modules/metaServer/lib/socialMedia/facebook/auth/getAppAccessToken.ts
+++ src/modules/metaServer/lib/socialMedia/facebook/auth/getAppAccessToken.ts
@@ -5,14 +5,19 @@ import { getFacebookAppId, getFacebookAppSecret } from '../config'
 const tokenUrl = 'https://graph.facebook.com/oauth/access_token'
 const grant_type = 'client_credentials'
 
+interface AppAccessTokenResponse {
+  access_token: string
+}
+
 /**
- * Obtains an access token for the app
+ * Obtains an access token for the app.
+ * https://developers.facebook.com/docs/facebook-login/guides/access-tokens#generating-an-app-access-token
  * @returns An access token for the app
  */
-export const getAppAccessToken = async () => {
+export const getAppAccessToken = async (): Promise<string | undefined> => {
   const client_id = getFacebookAppId()
   const client_secret = getFacebookAppSecret()
   const params = { client_id, client_secret, grant_type }
-  const response = await axios.get(tokenUrl, { params })
+  const response = await axios.get<AppAccessTokenResponse>(tokenUrl, { params })
   return response.data.access_token
 }

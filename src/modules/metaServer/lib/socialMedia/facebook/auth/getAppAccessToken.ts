8e21cc2f1c43afc9b0f0e1a7c38749a9547f27e4
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-24 16:41:32 -0500

    Add docs

--- src/modules/metaServer/lib/socialMedia/facebook/auth/getAppAccessToken.ts
+++ src/modules/metaServer/lib/socialMedia/facebook/auth/getAppAccessToken.ts
@@ -5,6 +5,10 @@ import { getFacebookAppId, getFacebookAppSecret } from '../config'
 const tokenUrl = 'https://graph.facebook.com/oauth/access_token'
 const grant_type = 'client_credentials'
 
+/**
+ * Obtains an access token for the app
+ * @returns An access token for the app
+ */
 export const getAppAccessToken = async () => {
   const client_id = getFacebookAppId()
   const client_secret = getFacebookAppSecret()

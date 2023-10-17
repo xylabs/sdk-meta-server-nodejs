018877dc38737969145b841ec2be785763e1cd73
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-17 12:09:57 -0500

    Implementation notes

--- src/modules/metaServer/contentHandlers/liveShare/lib/image/getImageUrl.ts
+++ src/modules/metaServer/contentHandlers/liveShare/lib/image/getImageUrl.ts
@@ -1,5 +1,6 @@
 import { join, removeQueryParam } from '../../../../lib'
 
 export const getImageUrl = (url: string, width: string | number, height: string | number): string => {
+  // TODO: Only remove render if it's preview?
   return removeQueryParam(join(url, `${width}`, `${height}`, 'img.png'), 'render')
 }

16688f3b92ee6d32a3a27fd493ad582fb4085b20
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-17 05:11:57 -0500

    Use helpers to extract decoded HTML image preview URL

--- src/modules/metaServer/contentHandlers/liveShare/lib/pageStrategies/useIndexAndDeferredPreviewImage/useIndexAndDeferredPreviewImage.ts
+++ src/modules/metaServer/contentHandlers/liveShare/lib/pageStrategies/useIndexAndDeferredPreviewImage/useIndexAndDeferredPreviewImage.ts
@@ -1,9 +1,10 @@
 import { assertEx } from '@xylabs/assert'
 import { metaBuilder } from '@xyo-network/sdk-meta'
+import { decode } from 'he'
 
-import { useSpaPage } from '../../../../../lib'
 import { ImageCache } from '../../cache'
 import { getPagePreviewImageMeta, getRenderedPageAsImage } from '../../image'
+import { getRenderedPageHtml } from '../getRenderedPageHtml'
 
 /**
  * The property name of the meta element
@@ -11,30 +12,40 @@ import { getPagePreviewImageMeta, getRenderedPageAsImage } from '../../image'
  */
 const xyoOgImageProperty = 'xyo:og:image'
 
+// Define the regex pattern to match the meta element
+const regexPattern = /<meta[^>]*property="xyo:og:image"[^>]*content="([^"]*)"[^>]*>/
+
 export const useIndexAndDeferredPreviewImage = async (url: string, imageCache: ImageCache, indexHtml: string): Promise<string> => {
   try {
     console.log(`[liveShare][useIndexAndDeferredPreviewImage][${url}]: rendering in background`)
     // TODO: Optimize this with something like React SSR
     // Parse head for xyo:og:image data URL
-    const metaImageElement = assertEx(
-      await useSpaPage(url, async (page) => {
-        console.log(`[liveShare][getRenderedPageHtml][${url}]: navigated to ${url}`)
-        return await page.evaluate(() => {
-          // Find the first meta element with the specified property
-          const meta = document.head.querySelector(`meta[property="${xyoOgImageProperty}"]`)
-          // Extract the meta element's attributes
-          if (meta) {
-            const content = meta.getAttribute('content')
-            if (content) {
-              return { content, property: xyoOgImageProperty }
-            }
-          }
-          return undefined
-        })
-      }),
-      `[liveShare][useIndexAndDeferredPreviewImage][${url}]: error, missing meta element with ${xyoOgImageProperty} property`,
+    // const metaImageElement = assertEx(
+    //   await usePage(url, undefined, async (page) => {
+    //     console.log(`[liveShare][getRenderedPageHtml][${url}]: navigated to ${url}`)
+    //     return await page.evaluate(() => {
+    //       // Find the first meta element with the specified property
+    //       const meta = document.head.querySelector(`meta[property="${xyoOgImageProperty}"]`)
+    //       // Extract the meta element's attributes
+    //       if (meta) {
+    //         const content = meta.getAttribute('content')
+    //         if (content) {
+    //           return { content, property: xyoOgImageProperty }
+    //         }
+    //       }
+    //       return undefined
+    //     })
+    //   }),
+    //   `[liveShare][useIndexAndDeferredPreviewImage][${url}]: error, missing meta element with ${xyoOgImageProperty} property`,
+    // )
+    // const previewUrl = metaImageElement.content
+    const html = assertEx(await getRenderedPageHtml(url), `[liveShare][useIndexAndDeferredPreviewImage][${url}]: error retrieving html`)
+    // Use the regex to extract the expected meta element
+    const match = html.match(regexPattern)
+    // Extract the preview image URL from the meta element & decode it
+    const previewUrl = decode(
+      assertEx(match?.[1], `[liveShare][useIndexAndDeferredPreviewImage][${url}]: error, missing meta element with ${xyoOgImageProperty} property`),
     )
-    const previewUrl = metaImageElement.content
     getRenderedPageAsImage(previewUrl, imageCache)
     const meta = getPagePreviewImageMeta(url)
     const updatedHtml = metaBuilder(indexHtml, meta)

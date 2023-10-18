e83495901334d93cf8b1241d8bad529b862dfd21
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-18 09:15:51 -0500

    Update logging

--- src/modules/metaServer/contentHandlers/liveShare/lib/pageStrategies/useIndexAndDeferredPreviewImage/useIndexAndDeferredPreviewImage.ts
+++ src/modules/metaServer/contentHandlers/liveShare/lib/pageStrategies/useIndexAndDeferredPreviewImage/useIndexAndDeferredPreviewImage.ts
@@ -5,11 +5,13 @@ import { getLiveSharePreviewUrlFromHtmlMeta, getPagePreviewImageMeta, getRendere
 
 export const useIndexAndDeferredPreviewImage = async (url: string, imageCache: ImageCache, indexHtml: string): Promise<string> => {
   try {
-    console.log(`[liveShare][useIndexAndDeferredPreviewImage][${url}]: rendering in background`)
+    console.log(`[liveShare][useIndexAndDeferredPreviewImage][${url}]: getting preview URL from page`)
     // Extract the preview image URL from the meta element & decode it
     const previewUrl = await getLiveSharePreviewUrlFromHtmlMeta(url)
+    console.log(`[liveShare][useIndexAndDeferredPreviewImage][${url}]: rendering in background`)
     // Initiate the image generation but don't await it
     getRenderedPageAsImage(previewUrl, imageCache)
+    console.log(`[liveShare][useIndexAndDeferredPreviewImage][${url}]: generating preview image meta`)
     const meta = getPagePreviewImageMeta(url)
     // TODO: Use page meta since we already took the hit to render
     const updatedHtml = metaBuilder(indexHtml, meta)

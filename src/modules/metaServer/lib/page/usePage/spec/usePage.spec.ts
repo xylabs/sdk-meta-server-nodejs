35d1d900ea128232bdb10ceacdd91903b51dd0ff
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-18 07:10:43 -0500

    Add usePage tests

---
+++ src/modules/metaServer/lib/page/usePage/spec/usePage.spec.ts
@@ -0,0 +1,24 @@
+import { usePage } from '../usePage'
+
+describe('usePage', () => {
+  const uri = 'https://xyo.network/brand'
+  const expected = '<title>XYO: Brand Assets &amp; Logos</title>'
+  describe('with navigateToRootFirst=false', () => {
+    it('gets the page', async () => {
+      const result = await usePage(uri, undefined, async (page) => {
+        await page.waitForSelector('title', { timeout: 10000 })
+        return page.content()
+      })
+      expect(result).toContain(expected)
+    }, 60000)
+  })
+  describe('with navigateToRootFirst=true', () => {
+    it('gets the page', async () => {
+      const result = await usePage(uri, undefined, async (page) => {
+        await page.waitForSelector('title', { timeout: 10000 })
+        return page.content()
+      })
+      expect(result).toContain(expected)
+    }, 60000)
+  })
+})

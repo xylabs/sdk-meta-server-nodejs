7272d8f7d463645992bcc3372b124c70fe785bf9
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-24 17:01:33 -0500

    Towards matching browser file API

--- src/modules/metaServer/lib/aws/s3/repository/S3FileRepository.ts
+++ src/modules/metaServer/lib/aws/s3/repository/S3FileRepository.ts
@@ -4,24 +4,46 @@ import { lookup } from 'mime-types'
 import { S3Store } from '../store'
 
 /**
- * A file to be stored in S3.
+ * A file to be stored in the repository.
  */
 export interface File {
   /**
-   * The content type of the file. If not provided, we'll try to determine it from the file ID.
-   */
-  contentType?: string
-  /**
    * The file data.
    */
-  data: Uint8Array
+  data: ArrayBuffer
+  /**
+   * The content type of the file. If not provided, we'll try to determine it from the file ID.
+   */
+  readonly type?: string
   /**
    * The file URI with extension.
    */
-  uri: string
+  readonly uri: string
 }
 
-export class S3FileRepository {
+/**
+ * A repository for storing files.
+ */
+export interface FileRepository {
+  /**
+   * Adds a file to the repository.
+   * @param file The file to add.
+   */
+  addFile(file: File): Promise<void>
+  /**
+   * Finds a file in the repository.
+   * @param uri The URI of the file to find.
+   * @returns The file, if found. Otherwise, undefined.
+   */
+  findFile(uri: string): Promise<File | undefined>
+  /**
+   * Removes a file from the repository.
+   * @param uri The URI of the file to remove.
+   */
+  removeFile(uri: string): Promise<void>
+}
+
+export class S3FileRepository implements FileRepository {
   private store: S3Store
 
   constructor(bucketName: string, config: S3ClientConfig) {
@@ -33,12 +55,11 @@ export class S3FileRepository {
    * @param file The file to add.
    */
   async addFile(file: File): Promise<void> {
+    const key = file.uri
+    const value = new Uint8Array(file.data)
     // If contentType isn't provided, determine it from the file uri (which should have the extension)
-    if (!file.contentType) {
-      const contentType = lookup(file.uri) || 'application/octet-stream'
-      file.contentType = contentType
-    }
-    await this.store.set(file.uri, file.data, file.contentType)
+    const type = file.type || lookup(file.uri) || 'application/octet-stream'
+    await this.store.set(key, value, type)
   }
 
   /**
@@ -47,12 +68,14 @@ export class S3FileRepository {
    * @returns The file, if found. Otherwise, undefined.
    */
   async findFile(uri: string): Promise<File | undefined> {
-    const data = await this.store.get(uri)
-    if (!data) {
+    const result = await this.store.get(uri)
+    if (!result) {
       return undefined
     }
-    const contentType = lookup(uri) || 'application/octet-stream'
-    return { contentType, data, uri }
+    const type = lookup(uri) || 'application/octet-stream'
+    const data: ArrayBuffer = result.buffer
+    const file: File = { data, type, uri }
+    return file
   }
 
   /**

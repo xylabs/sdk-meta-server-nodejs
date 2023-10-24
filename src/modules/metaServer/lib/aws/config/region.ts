868d3943c722775bed8771c7aa9f9630d8b2e26d
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-24 10:10:04 -0500

    Default region via ENV VAR

--- src/modules/metaServer/lib/aws/config/region.ts
+++ src/modules/metaServer/lib/aws/config/region.ts
@@ -1,19 +1,19 @@
 import { getEnvVar, hasEnvVar } from '../../env'
 
 /**
- * Checks if the AWS_REGION environment variable is defined
- * @returns true if the AWS_REGION environment variable is defined
+ * Checks if the AWS_DEFAULT_REGION environment variable is defined
+ * @returns true if the AWS_DEFAULT_REGION environment variable is defined
  */
-export const hasRegion = (): boolean => hasEnvVar('AWS_REGION')
+export const hasRegion = (): boolean => hasEnvVar('AWS_DEFAULT_REGION')
 
 /**
- * Checks if the AWS_REGION environment variable is defined
- * @returns true if the AWS_REGION environment variable is defined
+ * Checks if the AWS_DEFAULT_REGION environment variable is defined
+ * @returns true if the AWS_DEFAULT_REGION environment variable is defined
  */
-export const getRegion = (): string | undefined => getEnvVar('AWS_REGION')
+export const getRegion = (): string | undefined => getEnvVar('AWS_DEFAULT_REGION')
 
 /**
- * Checks if the AWS_REGION environment variable is defined
- * @returns true if the AWS_REGION environment variable is defined
+ * Checks if the AWS_DEFAULT_REGION environment variable is defined
+ * @returns true if the AWS_DEFAULT_REGION environment variable is defined
  */
 export const getRegionOrDefault = (): string | undefined => getRegion() || 'us-east-1'

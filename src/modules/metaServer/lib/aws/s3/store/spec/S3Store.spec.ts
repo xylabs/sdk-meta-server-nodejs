a755ea14c783acdf7cb82cd8cffd9867448ba17e
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-24 09:18:20 -0500

    Placeholder for tests against real content types

--- src/modules/metaServer/lib/aws/s3/store/spec/S3Store.spec.ts
+++ src/modules/metaServer/lib/aws/s3/store/spec/S3Store.spec.ts
@@ -1,5 +1,6 @@
 import { S3ClientConfig } from '@aws-sdk/client-s3'
 import { describeIf } from '@xylabs/jest-helpers'
+import { join } from 'path'
 
 import { getAwsS3ClientConfig } from '../../getAwsS3ClientConfig'
 import { hasAwsS3ClientConfig } from '../../hasAwsS3ClientConfig'
@@ -13,67 +14,69 @@ describeIf(hasAwsS3ClientConfig())('S3Store', () => {
 
   // Helper function to generate unique keys for testing
   const generateUniqueKey = (): string => `test-key-${Date.now()}-${Math.random()}`
+  const cases: [file: string, contentType: string, contentEncoding: string][] = [
+    // TODO: Add file types here for image, html, etc.
+    [join(__dirname, 'index.html'), 'text/html', 'gzip'],
+  ]
 
-  beforeAll(() => {
-    config = getAwsS3ClientConfig()
-  })
-  beforeEach(() => {
-    sut = new S3Store(TEST_BUCKET, config)
-  })
-
-  describe('delete', () => {
-    it('should delete an object', async () => {
-      const testKey = generateUniqueKey()
-
-      // First, set a value
-      await sut.set(testKey, new Uint8Array([1, 2, 3]))
-
-      // Now, delete it
-      await sut.delete(testKey)
-
-      // Check if it's really deleted
-      const result = await sut.get(testKey)
-      expect(result).toBeUndefined()
+  describe.each(cases)('set', (file, contentType, contentEncoding) => {
+    beforeAll(() => {
+      config = getAwsS3ClientConfig()
     })
-  })
-
-  describe('get', () => {
-    it('should get an object', async () => {
-      const testKey = generateUniqueKey()
-      const testData = new Uint8Array([1, 2, 3])
-
-      await sut.set(testKey, testData)
-
-      const result = await sut.get(testKey)
-
-      expect(result).toEqual(testData)
+    beforeEach(() => {
+      sut = new S3Store(TEST_BUCKET, config)
     })
+    describe('delete', () => {
+      it('should delete an object', async () => {
+        const testKey = generateUniqueKey()
 
-    it('should return undefined when object is not found', async () => {
-      const result = await sut.get(generateUniqueKey())
-      expect(result).toBeUndefined()
+        // First, set a value
+        await sut.set(testKey, new Uint8Array([1, 2, 3]))
+
+        // Now, delete it
+        await sut.delete(testKey)
+
+        // Check if it's really deleted
+        const result = await sut.get(testKey)
+        expect(result).toBeUndefined()
+      })
     })
-  })
+    describe('get', () => {
+      it('should get an object', async () => {
+        const testKey = generateUniqueKey()
+        const testData = new Uint8Array([1, 2, 3])
 
-  describe('set', () => {
-    it('should set an object', async () => {
-      const testKey = generateUniqueKey()
-      const testData = new Uint8Array([1, 2, 3])
+        await sut.set(testKey, testData)
 
-      await sut.set(testKey, testData)
+        const result = await sut.get(testKey)
 
-      const result = await sut.get(testKey)
-      expect(result).toEqual(testData)
+        expect(result).toEqual(testData)
+      })
+
+      it('should return undefined when object is not found', async () => {
+        const result = await sut.get(generateUniqueKey())
+        expect(result).toBeUndefined()
+      })
     })
+    describe('set', () => {
+      it('should set an object', async () => {
+        const testKey = generateUniqueKey()
+        const testData = new Uint8Array([1, 2, 3])
 
-    it('should delete an object if value is not provided', async () => {
-      const testKey = generateUniqueKey()
-      await sut.set(testKey, new Uint8Array([1, 2, 3]))
+        await sut.set(testKey, testData)
 
-      await sut.set(testKey, undefined)
+        const result = await sut.get(testKey)
+        expect(result).toEqual(testData)
+      })
+      it('should delete an object if value is not provided', async () => {
+        const testKey = generateUniqueKey()
+        await sut.set(testKey, new Uint8Array([1, 2, 3]))
 
-      const result = await sut.get(testKey)
-      expect(result).toBeUndefined()
+        await sut.set(testKey, undefined)
+
+        const result = await sut.get(testKey)
+        expect(result).toBeUndefined()
+      })
     })
   })
 })

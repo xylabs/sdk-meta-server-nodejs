8e770d0e091d44605498ab0f197ba582036aa102
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-24 06:21:06 -0500

    Update tests to match store signature

--- src/modules/metaServer/lib/s3/store/spec/S3Store.spec.ts
+++ src/modules/metaServer/lib/s3/store/spec/S3Store.spec.ts
@@ -1,12 +1,12 @@
 import { S3ClientConfig } from '@aws-sdk/client-s3'
 
-import { S3Cache } from '../S3Store'
+import { S3Store } from '../S3Store'
 
 const TEST_BUCKET = 'your-test-bucket'
 
-describe.skip('S3Cache', () => {
+describe.skip('S3Store', () => {
   let config: S3ClientConfig
-  let s3Cache: S3Cache
+  let sut: S3Store
 
   // Helper function to generate unique keys for testing
   const generateUniqueKey = (): string => `test-key-${Date.now()}-${Math.random()}`
@@ -24,7 +24,7 @@ describe.skip('S3Cache', () => {
     }
   })
   beforeEach(() => {
-    s3Cache = new S3Cache(TEST_BUCKET, config)
+    sut = new S3Store(TEST_BUCKET, config)
   })
 
   describe('delete', () => {
@@ -32,13 +32,13 @@ describe.skip('S3Cache', () => {
       const testKey = generateUniqueKey()
 
       // First, set a value
-      await s3Cache.set(testKey, Promise.resolve(new Uint8Array([1, 2, 3])))
+      await sut.set(testKey, new Uint8Array([1, 2, 3]))
 
       // Now, delete it
-      await s3Cache.delete(testKey)
+      await sut.delete(testKey)
 
       // Check if it's really deleted
-      const result = await s3Cache.get(testKey)
+      const result = await sut.get(testKey)
       expect(result).toBeUndefined()
     })
   })
@@ -48,15 +48,15 @@ describe.skip('S3Cache', () => {
       const testKey = generateUniqueKey()
       const testData = new Uint8Array([1, 2, 3])
 
-      await s3Cache.set(testKey, Promise.resolve(testData))
+      await sut.set(testKey, testData)
 
-      const result = await s3Cache.get(testKey)
+      const result = await sut.get(testKey)
 
       expect(result).toEqual(testData)
     })
 
     it('should return undefined when object is not found', async () => {
-      const result = await s3Cache.get(generateUniqueKey())
+      const result = await sut.get(generateUniqueKey())
       expect(result).toBeUndefined()
     })
   })
@@ -66,19 +66,19 @@ describe.skip('S3Cache', () => {
       const testKey = generateUniqueKey()
       const testData = new Uint8Array([1, 2, 3])
 
-      await s3Cache.set(testKey, Promise.resolve(testData))
+      await sut.set(testKey, testData)
 
-      const result = await s3Cache.get(testKey)
+      const result = await sut.get(testKey)
       expect(result).toEqual(testData)
     })
 
     it('should delete an object if value is not provided', async () => {
       const testKey = generateUniqueKey()
-      await s3Cache.set(testKey, Promise.resolve(new Uint8Array([1, 2, 3])))
+      await sut.set(testKey, new Uint8Array([1, 2, 3]))
 
-      await s3Cache.set(testKey, undefined)
+      await sut.set(testKey, undefined)
 
-      const result = await s3Cache.get(testKey)
+      const result = await sut.get(testKey)
       expect(result).toBeUndefined()
     })
   })

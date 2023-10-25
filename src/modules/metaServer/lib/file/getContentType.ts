9be8bda30070a0747bf84c56477a3d8bec622a39
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-25 13:02:52 -0500

    Profile similar libraries

--- src/modules/metaServer/lib/file/getContentType.ts
+++ src/modules/metaServer/lib/file/getContentType.ts
@@ -1,5 +1,10 @@
 import mime from 'mime'
+import { lookup } from 'mime-types'
 
-export const getContentType = (filePath: string): string | null => {
-  return mime.getType(filePath)
+export const getContentType = (filePath: string): string | false => {
+  return mime.getType(filePath) ?? false
+}
+
+export const getContentTypeViaLookup = (filePath: string): string | false => {
+  return lookup(filePath)
 }
---
+++ src/modules/metaServer/lib/file/spec/getContentType.spec.ts
@@ -0,0 +1,35 @@
+import { getContentType, getContentTypeViaLookup } from '../getContentType'
+
+const pathsWithRealExtensions = [
+  'index.html',
+  'index.HTML',
+  'bundle.js',
+  'image.jpg',
+  'image.gif',
+  'image.webp',
+  'https://beta.explore.xyo.network/favicon.ico',
+  '/K2FifZFYk-dHSE0UPPuwQ7CrD94i-NCKm-U48M1wqxnDirBPlw.woff2',
+  'https://cdn.xy.company/img/brand/XYO/XYO_icon_white.svg',
+]
+
+const pathsWithFakeExtensions = [
+  'network.xyo.payload',
+  'network.xyo.schema',
+  'network.xyo.location.result',
+  'network.xyo.location.result',
+  'foo.bar',
+  'baz.qix',
+  'https://beta.api.archivist.xyo.network/archive/temp/payload/hash/32d377bfe7ebe382598c54dd13f8af7510e0a1e2fd2e913311fdd58e517e5e2e/',
+  'https://beta.explore.xyo.network/schema/network.xyo.domain',
+]
+
+describe.each([getContentType, getContentTypeViaLookup])('getContentType', (sut) => {
+  it.each(pathsWithRealExtensions)('Returns string for known file extensions', (uri) => {
+    const actual = sut(uri)
+    expect(actual).toBeString()
+    console.log(actual)
+  })
+  it.each(pathsWithFakeExtensions)('Returns false for unrecognized file extensions', (uri) => {
+    expect(sut(uri)).toBe(false)
+  })
+})

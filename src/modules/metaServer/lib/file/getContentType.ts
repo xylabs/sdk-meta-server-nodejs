d3c2e9921166fe277fc2bd5711c0e30ef6c1775a
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-25 13:08:22 -0500

    Remove duplicate library

--- src/modules/metaServer/lib/file/getContentType.ts
+++ src/modules/metaServer/lib/file/getContentType.ts
@@ -1,10 +1,5 @@
 import mime from 'mime'
-import { lookup } from 'mime-types'
 
 export const getContentType = (filePath: string): string | false => {
   return mime.getType(filePath) ?? false
-}
-
-export const getContentTypeViaLookup = (filePath: string): string | false => {
-  return lookup(filePath)
 }

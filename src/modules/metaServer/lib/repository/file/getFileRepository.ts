399fb5e4b86cf3dc0418ee7e2fd1449eff8593f3
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-24 22:28:12 -0500

    Comments & documentation

--- src/modules/metaServer/lib/repository/file/getFileRepository.ts
+++ src/modules/metaServer/lib/repository/file/getFileRepository.ts
@@ -3,6 +3,11 @@ import { FileRepository } from './FileRepository'
 import { MemoryFileRepository } from './memory'
 import { S3FileRepository } from './s3'
 
+/**
+ * Gets a file repository. If AWS S3 client config is provided, an S3 file
+ * repository is returned. Otherwise, a memory file repository is returned.
+ * @returns A file repository
+ */
 export const getFileRepository: () => FileRepository = () => {
   if (hasAwsS3ClientConfig()) {
     return new S3FileRepository(getDefaultBucket(), getAwsS3ClientConfig())

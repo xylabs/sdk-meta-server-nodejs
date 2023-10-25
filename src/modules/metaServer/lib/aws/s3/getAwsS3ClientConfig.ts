4a6e0b1b7c2982ac571bf925be0a34b374ee5f6e
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-25 09:12:56 -0500

    Use semantic helpers

--- src/modules/metaServer/lib/aws/s3/getAwsS3ClientConfig.ts
+++ src/modules/metaServer/lib/aws/s3/getAwsS3ClientConfig.ts
@@ -1,13 +1,12 @@
 import { S3ClientConfig } from '@aws-sdk/client-s3'
-import { assertEx } from '@xylabs/assert'
 
-import { getRegionOrDefault, tryGetAccessKeyId, tryGetSecretAccessKey } from '../config'
+import { getAccessKeyId, getRegionOrDefault, getSecretAccessKey } from '../config'
 
 export const getAwsS3ClientConfig: () => S3ClientConfig = () => {
   return {
     credentials: {
-      accessKeyId: assertEx(tryGetAccessKeyId()),
-      secretAccessKey: assertEx(tryGetSecretAccessKey()),
+      accessKeyId: getAccessKeyId(),
+      secretAccessKey: getSecretAccessKey(),
     },
     region: getRegionOrDefault(),
   }

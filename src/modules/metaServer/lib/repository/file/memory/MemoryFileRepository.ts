02e9f5769665c774404e584e212df1f732c9730f
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-24 22:08:55 -0500

    Memory File Repository

--- src/modules/metaServer/lib/repository/file/memory/MemoryFileRepository.ts
+++ src/modules/metaServer/lib/repository/file/memory/MemoryFileRepository.ts
@@ -1,3 +1,14 @@
-export class MemoryFileRepository {
-  // TODO: Implement this class
+import { FileRepository } from '../FileRepository'
+import { RepositoryFile } from '../RepositoryFile'
+
+export class MemoryFileRepository implements FileRepository {
+  addFile(file: RepositoryFile): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+  findFile(uri: string): Promise<RepositoryFile | undefined> {
+    throw new Error('Method not implemented.')
+  }
+  removeFile(uri: string): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
 }

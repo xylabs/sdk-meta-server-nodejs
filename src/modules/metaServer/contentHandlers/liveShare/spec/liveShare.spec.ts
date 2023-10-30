bea711714e4c82cc0cebe9d1bdf5a4ca190577c3
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-30 16:47:57 -0500

    Live share tests

--- src/modules/metaServer/contentHandlers/liveShare/spec/liveShare.spec.ts
+++ src/modules/metaServer/contentHandlers/liveShare/spec/liveShare.spec.ts
@@ -1,22 +1,26 @@
+import { Server } from 'http'
 import { StatusCodes } from 'http-status-codes'
 import { SuperTest, Test } from 'supertest'
 
-import { getServer } from '../../../spec'
+import { getServerOnPort } from '../../../spec'
 
 describe('liveShare', () => {
-  let server: SuperTest<Test>
+  let server: Server
+  let testClient: SuperTest<Test>
   beforeEach(() => {
     // Serve up this directory
-    server = getServer(__dirname)
+    ;[server, testClient] = getServerOnPort(12345, __dirname)
+  })
+  afterAll((done) => {
+    server.close(() => done())
   })
   describe('when requested resource exists', () => {
     it('serves up the original file content unmodified', async () => {
       // Get this file from the server
-      const response = await server.get('/index.html').expect(StatusCodes.OK)
-      expect(response.body).toBeTruthy()
-      console.log(response.body.toString())
-      const actual = response.body.toString()
-      expect(actual).toBeTruthy()
+      const response = await testClient.get('/other.html').expect(StatusCodes.OK)
+      const html = response.text
+      expect(html).toBeString()
+      console.log(html)
     })
   })
 })

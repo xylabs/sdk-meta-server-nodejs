2ac5e68f069236a3972713f62a3fda79ccfd7022
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-30 14:33:14 -0500

    Documentation

--- src/modules/metaServer/contentHandlers/liveShare/lib/image/getImageUrl.ts
+++ src/modules/metaServer/contentHandlers/liveShare/lib/image/getImageUrl.ts
@@ -1,5 +1,12 @@
 import { join, removeQueryParam } from '../../../../lib'
 
+/**
+ * Generates the url of the preview image for a Live Share page
+ * @param url The url for the page
+ * @param width The preview image width
+ * @param height The preview image height
+ * @returns The url of the preview image for a Live Share page
+ */
 export const getImageUrl = (url: string, width: string | number, height: string | number): string => {
   // TODO: Only remove render if it's preview?
   return removeQueryParam(join(url, `${width}`, `${height}`, 'img.png'), 'render')
--- src/modules/metaServer/contentHandlers/liveShare/lib/image/getPagePreviewImageMeta.ts
+++ src/modules/metaServer/contentHandlers/liveShare/lib/image/getPagePreviewImageMeta.ts
@@ -3,6 +3,13 @@ import { Meta, OpenGraphMeta, TwitterMeta } from '@xyo-network/sdk-meta'
 import { defaultViewportSize, join } from '../../../../lib'
 import { getImageUrl } from './getImageUrl'
 
+/**
+ * Generates the expected metadata for the preview image of a Live Share page
+ * @param url The url for the page
+ * @param width The height of the preview image
+ * @param height The width of the preview image
+ * @returns The expected metadata for the preview image of a Live Share page
+ */
 export const getPagePreviewImageMeta = (url: string, width = defaultViewportSize.width, height = defaultViewportSize.height): Meta => {
   console.log(`[liveShare][getPagePreviewImageMeta][${url}]: generating`)
   const previewUrl = join(url, 'preview')

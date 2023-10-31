478441f10fc8ec23ea84a74bb6eea94573bce917
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-31 17:39:51 -0500

    Log repository type

--- src/modules/metaServer/lib/repository/file/getFileRepository.ts
+++ src/modules/metaServer/lib/repository/file/getFileRepository.ts
@@ -3,7 +3,7 @@ import { FileRepository } from './FileRepository'
 import { MemoryFileRepository } from './memory'
 import { S3FileRepository } from './s3'
 
-const repository: FileRepository = hasBucket() ? new S3FileRepository(getBucket(), getAwsS3ClientConfig()) : new MemoryFileRepository()
+let repository: FileRepository | undefined
 
 /**
  * Gets a file repository. If AWS S3 client config is provided, an S3 file
@@ -11,5 +11,14 @@ const repository: FileRepository = hasBucket() ? new S3FileRepository(getBucket(
  * @returns A file repository
  */
 export const getFileRepository: () => FileRepository = () => {
+  if (repository === undefined) {
+    if (hasBucket()) {
+      console.log('Using S3 file repository')
+      repository = new S3FileRepository(getBucket(), getAwsS3ClientConfig())
+    } else {
+      console.log('Using Memory file repository')
+      repository = new MemoryFileRepository()
+    }
+  }
   return repository
 }

983e0d17f645904a59305bdfa61aae196cc811c5
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-31 17:35:05 -0500

    Conditional AWS config builder

--- src/modules/metaServer/lib/aws/s3/getAwsS3ClientConfig.ts
+++ src/modules/metaServer/lib/aws/s3/getAwsS3ClientConfig.ts
@@ -1,13 +1,16 @@
 import { S3ClientConfig } from '@aws-sdk/client-s3'
 
-import { getAccessKeyId, getRegionOrDefault, getSecretAccessKey } from '../config'
+import { getAccessKeyId, getRegionOrDefault, getSecretAccessKey, hasAccessKeyId, hasSecretAccessKey } from '../config'
 
 export const getAwsS3ClientConfig: () => S3ClientConfig = () => {
-  return {
-    credentials: {
-      accessKeyId: getAccessKeyId(),
-      secretAccessKey: getSecretAccessKey(),
-    },
+  const config: S3ClientConfig = {
     region: getRegionOrDefault(),
   }
+  if (hasAccessKeyId() && hasSecretAccessKey()) {
+    config.credentials = {
+      accessKeyId: getAccessKeyId(),
+      secretAccessKey: getSecretAccessKey(),
+    }
+  }
+  return config
 }

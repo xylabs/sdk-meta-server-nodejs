674a8769cde7fb7646ea1e0cd380003755e3e38e
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-31 13:28:08 -0500

    Tests for page preview meta generation

--- src/modules/metaServer/contentHandlers/liveShare/lib/image/getPagePreviewImageMeta.ts
+++ src/modules/metaServer/contentHandlers/liveShare/lib/image/getPagePreviewImageMeta.ts
@@ -12,11 +12,10 @@ import { getImageUrl } from './getImageUrl'
  */
 export const getPagePreviewImageMeta = (url: string, width = defaultViewportSize.width, height = defaultViewportSize.height): Meta => {
   console.log(`[liveShare][getPagePreviewImageMeta][${url}]: generating`)
-  const previewUrl = join(url, 'preview')
-  const imageUrl = getImageUrl(previewUrl, width, height)
+  const imageUrl = getImageUrl(join(url, 'preview'), width, height)
   const og: OpenGraphMeta = { image: { '': imageUrl, height, secure_url: imageUrl, type: 'image/png', url: imageUrl, width } }
   const twitter: TwitterMeta = { card: 'summary_large_image', image: { '': imageUrl } }
-  const meta: Meta = { og, twitter }
+  const meta = { og, twitter }
   console.log(`[liveShare][getPagePreviewImageMeta][${url}]: generated`)
   return meta
 }

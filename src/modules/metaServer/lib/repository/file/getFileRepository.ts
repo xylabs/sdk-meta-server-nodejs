fa7ebf4f36104ee2c1d62535656c4d7117038608
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-31 17:35:35 -0500

    Pivot cache type on bucket presence

--- src/modules/metaServer/lib/repository/file/getFileRepository.ts
+++ src/modules/metaServer/lib/repository/file/getFileRepository.ts
@@ -1,11 +1,9 @@
-import { getAwsS3ClientConfig, getDefaultBucket, hasAwsS3ClientConfig } from '../../aws'
+import { getAwsS3ClientConfig, getBucket, hasBucket } from '../../aws'
 import { FileRepository } from './FileRepository'
 import { MemoryFileRepository } from './memory'
 import { S3FileRepository } from './s3'
 
-const repository: FileRepository = hasAwsS3ClientConfig()
-  ? new S3FileRepository(getDefaultBucket(), getAwsS3ClientConfig())
-  : new MemoryFileRepository()
+const repository: FileRepository = hasBucket() ? new S3FileRepository(getBucket(), getAwsS3ClientConfig()) : new MemoryFileRepository()
 
 /**
  * Gets a file repository. If AWS S3 client config is provided, an S3 file

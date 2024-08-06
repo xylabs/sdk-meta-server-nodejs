4aea28f0c2a142f97f70317f6deccc97d3e1db1d
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-08-06 17:43:46 -0500

    Use element polling during test

--- src/modules/metaServer/lib/page/usePage/spec/useSpaPage.spec.ts
+++ src/modules/metaServer/lib/page/usePage/spec/useSpaPage.spec.ts
@@ -7,29 +7,17 @@ const waitForElementToInclude = (selector: string, expectedValue: string) => {
 
 describe('useSpaPage', () => {
   const uri = 'https://xyo.network/brand'
-  const expected = '<title>XYO: Brand Assets &amp; Logos</title>'
-  describe('with navigateToRootFirst=false', () => {
-    it('gets the page', async () => {
-      const content = await useSpaPage(uri, async (page) => {
-        await page.waitForFunction(() => {
-          const element = document.querySelector('title')
-          return element && element.textContent?.includes('XYO: Brand Assets')
-        })
-        return page.content()
-      })
-      expect(content).toContain(expected)
-    }, 60_000)
-  })
+  const expected = 'XYO: Brand Assets'
   describe('with navigateToRootFirst=true', () => {
     it('gets the page', async () => {
       const content = await useSpaPage(uri, async (page) => {
-        await page.waitForFunction((selector, expectedValue) => {
-          const element = document.querySelector(selector)
-          return element && element.textContent?.includes(expectedValue)
-        },
-        {}, // Options object, like timeout
-        'title',
-        'XYO: Brand Assets')
+        await page.waitForFunction(waitForElementToInclude,
+          {
+            polling: 100,
+            timeout: 30_000,
+          },
+          'title',
+          expected)
         return page.content()
       })
       expect(content).toContain(expected)

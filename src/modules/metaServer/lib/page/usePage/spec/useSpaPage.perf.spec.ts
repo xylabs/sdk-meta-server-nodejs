3a22fed4bf2d426a91f47cd8f00586d0f02f1008
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-08-07 08:39:34 -0500

    Increase load

--- src/modules/metaServer/lib/page/usePage/spec/useSpaPage.perf.spec.ts
+++ src/modules/metaServer/lib/page/usePage/spec/useSpaPage.perf.spec.ts
@@ -1,27 +1,14 @@
 import { useSpaPage } from '../useSpaPage.js'
 
-const waitForElementToInclude = (selector: string, expectedValue: string) => {
-  const element = document.querySelector(selector)
-  return element && element.textContent?.includes(expectedValue)
-}
-
 describe('useSpaPage [perf]', () => {
   const uri = 'https://react.dev/learn'
   const expected = 'Quick Start'
   it('gets the page', async () => {
-    for (let index = 0; index < 100; index++) {
+    await Promise.all(Array.from({ length: 200 }).map(async () => {
       const content = await useSpaPage(uri, async (page) => {
-        await page.waitForFunction(() => {
-          return true
-        },
-        {
-          // polling: 100,
-          // timeout: 30_000,
-        },
-        )
-        return page.content()
+        return await page.content()
       })
       expect(content).toContain(expected)
-    }
+    }))
   }, 60_000)
 })

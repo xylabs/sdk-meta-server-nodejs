5794bcbbd783847a34ead4c4b511b0d4ca30bcb4
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-08-07 08:19:07 -0500

    Simplify page ready handler

--- src/modules/metaServer/lib/page/usePage/spec/useSpaPage.perf.spec.ts
+++ src/modules/metaServer/lib/page/usePage/spec/useSpaPage.perf.spec.ts
@@ -8,21 +8,20 @@ const waitForElementToInclude = (selector: string, expectedValue: string) => {
 describe('useSpaPage [perf]', () => {
   const uri = 'https://react.dev/learn'
   const expected = 'Quick Start'
-  describe('with navigateToRootFirst=true', () => {
-    it('gets the page', async () => {
-      for (let index = 0; index < 100; index++) {
-        const content = await useSpaPage(uri, async (page) => {
-          await page.waitForFunction(waitForElementToInclude,
-            {
-              // polling: 100,
-              // timeout: 30_000,
-            },
-            'title',
-            expected)
-          return page.content()
-        })
-        expect(content).toContain(expected)
-      }
-    }, 60_000)
-  })
+  it('gets the page', async () => {
+    for (let index = 0; index < 100; index++) {
+      const content = await useSpaPage(uri, async (page) => {
+        await page.waitForFunction(() => {
+          return true
+        },
+        {
+          // polling: 100,
+          // timeout: 30_000,
+        },
+        )
+        return page.content()
+      })
+      expect(content).toContain(expected)
+    }
+  }, 60_000)
 })

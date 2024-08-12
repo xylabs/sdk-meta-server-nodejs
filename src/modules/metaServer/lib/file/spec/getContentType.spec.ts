94a8b67710321eb6afec88bc73a64b8b8caeaf2b
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-08-12 04:43:09 -0500

    Assert expected content types

--- src/modules/metaServer/lib/file/spec/getContentType.spec.ts
+++ src/modules/metaServer/lib/file/spec/getContentType.spec.ts
@@ -1,17 +1,17 @@
 import { getContentType } from '../getContentType.ts'
 
 const pathsWithRealExtensions = [
-  'index.html',
-  'index.HTML',
-  'bundle.js',
-  'image.jpg',
-  'image.gif',
-  'image.webp',
-  'https://beta.explore.xyo.network/favicon.ico',
-  '/K2FifZFYk-dHSE0UPPuwQ7CrD94i-NCKm-U48M1wqxnDirBPlw.woff2',
-  'https://cdn.xy.company/img/brand/XYO/XYO_icon_white.svg',
-  'https://xyo-network.ghost.io/content/images/2024/07/Header--1-.jpg',
-  'https://xyo-network.ghost.io/content/images/2024/07/1-g53vd4ro7yuoxyh-a0zygw.png',
+  ['index.html', 'text/html'],
+  ['index.HTML', 'text/html'],
+  ['bundle.js', 'text/javascript'],
+  ['image.jpg', 'image/jpeg'],
+  ['image.gif', 'image/gif'],
+  ['image.webp', 'image/webp'],
+  ['https://beta.explore.xyo.network/favicon.ico', 'image/vnd.microsoft.icon'],
+  ['/K2FifZFYk-dHSE0UPPuwQ7CrD94i-NCKm-U48M1wqxnDirBPlw.woff2', 'font/woff2'],
+  ['https://cdn.xy.company/img/brand/XYO/XYO_icon_white.svg', 'image/svg+xml'],
+  ['https://xyo-network.ghost.io/content/images/2024/07/Header--1-.jpg', 'image/jpeg'],
+  ['https://xyo-network.ghost.io/content/images/2024/07/1-g53vd4ro7yuoxyh-a0zygw.png', 'image/png'],
 ]
 
 const pathsWithFakeExtensions = [
@@ -26,9 +26,10 @@ const pathsWithFakeExtensions = [
 ]
 
 describe('getContentType', () => {
-  it.each(pathsWithRealExtensions)('Returns string for known file extensions', (uri) => {
+  it.each(pathsWithRealExtensions)('Returns string for known file extensions', (uri, expected) => {
     const actual = getContentType(uri)
     expect(actual).toBeString()
+    expect(actual).toEqual(expected)
   })
   it.each(pathsWithFakeExtensions)('Returns false for unrecognized file extensions', (uri) => {
     expect(getContentType(uri)).toBe(false)

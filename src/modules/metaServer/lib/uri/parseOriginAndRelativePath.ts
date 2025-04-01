5e865d1c87437bf15d98d6622c683d837589c776
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-04-01 08:10:16 -0500

    Add tests & documentation

--- src/modules/metaServer/lib/uri/parseOriginAndRelativePath.ts
+++ src/modules/metaServer/lib/uri/parseOriginAndRelativePath.ts
@@ -1,4 +1,23 @@
-export const parseOriginAndRelativePath = (url: string) => {
+/**
+ * A type representing the origin and relative path of a URL.
+ */
+export type OriginAndRelativePath = {
+  /**
+   * The origin of the URL, including the protocol and host.
+   */
+  origin: string
+  /**
+   * The relative path of the URL, including the pathname and search parameters.
+   */
+  relativePath: string
+}
+
+/**
+ * Parses a string URL into a base URL and a relative path.
+ * @param url - The URL to parse
+ * @returns An object containing the origin and relative path of the URL
+ */
+export const parseOriginAndRelativePath = (url: string): OriginAndRelativePath => {
   const parsed = new URL(url)
   const {
     origin, pathname, search,

3451a0d6a7758c2f19aa5d374713807ee29c8eb3
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-04-01 15:43:42 -0500

    Simplify bench using helper

--- src/modules/metaServer/lib/page/usePage/spec/useSpaPage.bench.ts
+++ src/modules/metaServer/lib/page/usePage/spec/useSpaPage.bench.ts
@@ -5,9 +5,8 @@ import { useSpaPage } from '../useSpaPage.ts'
 
 describe('Benchmark useSpaPage', () => {
   const uri = 'https://beta.xyo.network/blog'
-  const metaTag = 'meta[property="xyo:og:image"]'
   bench('navigate to lightweight SPA page', async () => {
-    await Promise.all(Array.from({ length: 12 }).map(() => {
+    await Promise.all(Array.from({ length: 3 }).map(() => {
     // await Promise.all([
     //   'https://beta.xyo.network/blog',
     //   'https://beta.xyo.network/news',
@@ -16,11 +15,7 @@ describe('Benchmark useSpaPage', () => {
       return useSpaPage(
         uri,
         async (page: Page) => {
-          await page.waitForFunction((selector) => {
-            const el = document.querySelector(selector)
-            return el && el.getAttribute('content') && el.getAttribute('content')!.trim() !== ''
-          }, { timeout: 20_000 }, metaTag)
-          const imageUrl = await (await page.$(metaTag))?.evaluate(el => el.getAttribute('content'))
+          const imageUrl = await waitForImageMetaTag(page)
           console.log('Image URL:', imageUrl)
           expect(imageUrl).toBeTruthy()
         },

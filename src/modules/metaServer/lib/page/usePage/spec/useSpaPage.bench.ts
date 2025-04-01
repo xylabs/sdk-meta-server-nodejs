e7a0f413448ad3b29db12d739d8163e4f105bc1c
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-04-01 10:42:42 -0500

    Update bench test to wait for image

--- src/modules/metaServer/lib/page/usePage/spec/useSpaPage.bench.ts
+++ src/modules/metaServer/lib/page/usePage/spec/useSpaPage.bench.ts
@@ -5,19 +5,23 @@ import { useSpaPage } from '../useSpaPage.ts'
 
 describe('Benchmark useSpaPage', () => {
   const uri = 'https://beta.xyo.network/blog'
+  const expectedTitle = 'XYO | Blog'
+  const metaTag = 'meta[property="xyo:og:image"]'
   bench('navigate to lightweight SPA page', async () => {
-    await useSpaPage(
-      uri,
-      async (page: Page) => {
-        // Minimal page interaction for benchmarking
-        const title = await page.title()
-        if (!title) throw new Error('No title found')
-        console.log('Page title:', title)
-        return title
-      },
-    )
+    await Promise.all(Array.from({ length: 10 }).map(() => {
+      return useSpaPage(
+        uri,
+        async (page: Page) => {
+          await page.waitForSelector(metaTag, { timeout: 20_000 })
+          const imageUrl = await (await page.$(metaTag))?.evaluate(el => el.getAttribute('content'))
+          console.log('Image URL:', imageUrl)
+          // const title = await page.title()
+          // expect(title).toBe(expectedTitle)
+        },
+      )
+    }))
   }, {
-    warmupIterations: 2,
-    iterations: 10, // optional: specify how many times to run it
+    warmupIterations: 1,
+    iterations: 2,
   })
 })

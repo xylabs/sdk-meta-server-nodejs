7e4e3220d8e75dea23b1fe191b3d43999ebe0254
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-04-01 07:32:20 -0500

    Simplify caching headers

--- src/modules/metaServer/lib/headersFromCacheConfig.ts
+++ src/modules/metaServer/lib/headersFromCacheConfig.ts
@@ -12,43 +12,23 @@ export const headersFromCacheConfig = ({
     cacheControlParts.push(`max-age=${maxAge}`)
     result['Expires'] = new Date(Date.now() + maxAge * 1000).toUTCString() // Expires in 60 seconds
   }
-  if (sMaxAge !== undefined) {
-    cacheControlParts.push(`s-maxage=${sMaxAge}`)
-  }
+  if (sMaxAge !== undefined) cacheControlParts.push(`s-maxage=${sMaxAge}`)
+
   if (noCache) {
     cacheControlParts.push('no-cache')
   } else {
     cacheControlParts.push(privateOnly ? 'private' : 'public')
-    if (mustRevalidate) {
-      cacheControlParts.push('must-revalidate')
-    }
-    if (mustUnderstand) {
-      cacheControlParts.push('must-understand')
-    }
-    if (proxyRevalidate) {
-      cacheControlParts.push('proxy-revalidate')
-    }
-    if (noTransform) {
-      cacheControlParts.push('no-transform')
-    }
-    if (onlyIfCached) {
-      cacheControlParts.push('only-if-cached')
-    }
-    if (immutable) {
-      cacheControlParts.push('immutable')
-    }
-    if (noStore) {
-      cacheControlParts.push('no-store')
-    }
-    if (staleIfError !== undefined) {
-      cacheControlParts.push(`stale-if-error=${staleIfError}`)
-    }
-    if (staleWhileRevalidate !== undefined) {
-      cacheControlParts.push(`stale-while-revalidate=${staleWhileRevalidate}`)
-    }
+    if (mustRevalidate) cacheControlParts.push('must-revalidate')
+    if (mustUnderstand) cacheControlParts.push('must-understand')
+    if (proxyRevalidate) cacheControlParts.push('proxy-revalidate')
+    if (noTransform) cacheControlParts.push('no-transform')
+    if (onlyIfCached) cacheControlParts.push('only-if-cached')
+    if (immutable) cacheControlParts.push('immutable')
+    if (noStore) cacheControlParts.push('no-store')
+    if (staleIfError !== undefined) cacheControlParts.push(`stale-if-error=${staleIfError}`)
+    if (staleWhileRevalidate !== undefined) cacheControlParts.push(`stale-while-revalidate=${staleWhileRevalidate}`)
   }
-  if (cacheControlParts.length > 0) {
-    result['Cache-Control'] = cacheControlParts.join(', ')
-  }
+
+  if (cacheControlParts.length > 0) result['Cache-Control'] = cacheControlParts.join(', ')
   return result
 }

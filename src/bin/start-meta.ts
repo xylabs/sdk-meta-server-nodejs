997808542655c1c9105801af57cdb029d071e55f
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-07-17 08:09:11 -0500

    Remove ENV from AWS

--- src/bin/start-meta.ts
+++ src/bin/start-meta.ts
@@ -1,17 +1,6 @@
-import { getEnvFromAws, tryParseInt } from '@xylabs/express'
+import { tryParseInt } from '@xylabs/express'
 
 import { metaServer } from '../modules/index.ts'
 
-void (async () => {
-  const port = tryParseInt(process.env.PORT) || 8083
-  // If an AWS ARN was supplied for Secrets Manager
-  const awsEnvSecret = process.env.AWS_ENV_SECRET_ARN
-  if (awsEnvSecret) {
-    console.log('Bootstrapping ENV from AWS')
-    // Merge the values from AWS into the current ENV
-    // with AWS taking precedence
-    const awsEnv = await getEnvFromAws(awsEnvSecret)
-    Object.assign(process.env, awsEnv)
-  }
-  metaServer(port)
-})()
+const port = tryParseInt(process.env.PORT) || 8083
+metaServer(port)
